0da1d4eee525328e4e5d3656fce6fa613f6057c4
Richard <richard.yang.tong@gmail.com>
2021-06-23 11:37:07 -0400

    Added notes.

--- spring-demo-one/src/com/dragontalker/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/com/dragontalker/springdemo/HelloSpringApp.java
@@ -7,15 +7,17 @@ public class HelloSpringApp {
 	public static void main(String[] args) {
 		
 		// load the spring configuration file
-		ClassPathXmlApplicationContext contaxt = 
+		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
 		
 		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
 		
 		// close the context
-
+		context.close();
 	}
 
 }
