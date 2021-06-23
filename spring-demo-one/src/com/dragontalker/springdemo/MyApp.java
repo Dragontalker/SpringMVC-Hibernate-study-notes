9206523ad531a38820035d6ab328c6583a1354db
Richard <richard.yang.tong@gmail.com>
2021-06-23 11:01:41 -0400

    Added notes.

--- spring-demo-one/src/com/dragontalker/springdemo/MyApp.java
+++ spring-demo-one/src/com/dragontalker/springdemo/MyApp.java
@@ -5,7 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		// create the object
-		BaseballCoach theCoach = new BaseballCoach();
+		Coach theCoach = new BaseballCoach();
 		
 		//use the object
 		System.out.println(theCoach.getDailyWorkout());
