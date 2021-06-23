6126d356a564f3202cee1b17941d0366f01de246
Richard <richard.yang.tong@gmail.com>
2021-06-23 11:01:06 -0400

    Added notes.

--- spring-demo-one/src/com/dragontalker/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/dragontalker/springdemo/BaseballCoach.java
@@ -1,6 +1,6 @@
 package com.dragontalker.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach{
 	
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
