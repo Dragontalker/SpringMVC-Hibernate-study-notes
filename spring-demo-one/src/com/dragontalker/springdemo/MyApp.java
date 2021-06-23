c5ecf17086d0e101a9f2f5e66bd28daa6f299440
Richard <richard.yang.tong@gmail.com>
2021-06-23 10:59:00 -0400

    Added notes.

--- spring-demo-one/src/com/dragontalker/springdemo/MyApp.java
+++ spring-demo-one/src/com/dragontalker/springdemo/MyApp.java
@@ -3,8 +3,12 @@ package com.dragontalker.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// create the object
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		//use the object
+		System.out.println(theCoach.getDailyWorkout());
 	}
 
 }
