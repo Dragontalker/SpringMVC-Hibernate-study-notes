10130f570969a636669b150cc2dc44fd786ab883
Richard <richard.yang.tong@gmail.com>
2021-06-23 11:35:49 -0400

    Added notes.

--- spring-demo-one/src/com/dragontalker/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/com/dragontalker/springdemo/HelloSpringApp.java
@@ -1,10 +1,14 @@
 package com.dragontalker.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HelloSpringApp {
 
 	public static void main(String[] args) {
 		
 		// load the spring configuration file
+		ClassPathXmlApplicationContext contaxt = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve bean from spring container
 		
