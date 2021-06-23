925622b4c452b1f84fa1222f8f956d832c7e2c16
Richard <richard.yang.tong@gmail.com>
2021-06-23 17:27:02 -0400

    Added notes.

--- spring-demo-one/src/com/dragontalker/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/dragontalker/springdemo/CricketCoach.java
@@ -1,16 +1,20 @@
 package com.dragontalker.springdemo;
 
-public class CricketCoach implements FortuneService {
+public class CricketCoach implements Coach {
 	
 	private FortuneService fortuneService;
 
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
-
+	
 	@Override
-	public String getFortune() {
-		return fortuneService.getFortune();
+	public String getDailyWorkout() {
+		return "Practice fast bowling for 15 minutes";
 	}
 
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
 }
--- spring-demo-one/src/com/dragontalker/springdemo/HelloSpringApp.java
+++ spring-demo-one/src/com/dragontalker/springdemo/HelloSpringApp.java
@@ -12,12 +12,15 @@ public class HelloSpringApp {
 		
 		// retrieve bean from spring container
 		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach cricketCoach = context.getBean("myCricketCoach", Coach.class)
 		
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(cricketCoach.getDailyWorkout());
 		
 		// let's call our new method for fortuneService
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(cricketCoach.getDailyFortune());
 		
 		// close the context
 		context.close();
