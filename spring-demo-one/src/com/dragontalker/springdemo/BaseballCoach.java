3df784eb0eac244cdde0c58acd06048db5bbde5f
Richard <richard.yang.tong@gmail.com>
2021-06-23 17:08:44 -0400

    Added notes.

--- spring-demo-one/src/com/dragontalker/springdemo/BaseballCoach.java
+++ spring-demo-one/src/com/dragontalker/springdemo/BaseballCoach.java
@@ -5,6 +5,11 @@ public class BaseballCoach implements Coach{
 	// define a private filed for the dependency
 	private FortuneService fortuneService;
 	
+	// define a constructor for dependency injection
+	public BaseballCoach(FortuneService theFortuneService) {
+		this.fortuneService = theFortuneService;
+	}
+	
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
