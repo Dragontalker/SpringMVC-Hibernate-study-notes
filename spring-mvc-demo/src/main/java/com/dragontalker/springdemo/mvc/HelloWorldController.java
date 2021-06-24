0423d332d34d562c27107deb4aa39088994bf2d9
Richard <richard.yang.tong@gmail.com>
2021-06-24 18:07:36 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/HelloWorldController.java
@@ -1,5 +1,12 @@
 package com.dragontalker.springdemo.mvc;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HelloWorldController {
+	
+	// need a controller method to show the initial HTML form
+	
+	// need a controller method to process the HTML form
 
 }
