1b299e27b715f2fce3e58437b7162593fda21a30
Richard <richard.yang.tong@gmail.com>
2021-06-24 11:57:54 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.dragontalker.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	
 	@Autowired
+	@Qualifier("happyFortuneService")
 	private FortuneService fortuneService;
 
 	@Override
