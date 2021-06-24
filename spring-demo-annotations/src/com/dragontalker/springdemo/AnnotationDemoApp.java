bf6c0dad9b1f8ca63dda36add7be8debf1acdaad
Richard <richard.yang.tong@gmail.com>
2021-06-24 10:32:01 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/AnnotationDemoApp.java
@@ -4,6 +4,14 @@ public class AnnotationDemoApp {
 	
 	public static void main(String[] args) {
 		
+		// read spring config file
+		
+		// get the bean from spring container
+		
+		// call a method on the bean
+		
+		// close the context
+		
 	}
 
 }
