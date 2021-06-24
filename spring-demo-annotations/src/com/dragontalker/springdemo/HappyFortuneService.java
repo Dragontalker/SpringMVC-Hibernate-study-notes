98f91866681c3d470483db91033c2eeab6616390
Richard <richard.yang.tong@gmail.com>
2021-06-24 11:29:43 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/HappyFortuneService.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/HappyFortuneService.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class HappyFortuneService implements FortuneService {
+	
+	public HappyFortuneService() {
+		System.out.println(">> HappyFortuneService: inside default constructor");
+	}
 
 	@Override
 	public String getFortune() {
--- spring-demo-annotations/src/com/dragontalker/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/TennisCoach.java
@@ -8,8 +8,13 @@ public class TennisCoach implements Coach {
 	
 	@Autowired
 	private FortuneService fortuneService;
+	
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
 
 	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside setter method for fortuneService");
 		this.fortuneService = fortuneService;
 	}
 
