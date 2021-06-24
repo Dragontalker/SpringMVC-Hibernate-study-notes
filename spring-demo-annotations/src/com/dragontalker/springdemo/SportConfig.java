d8d26315599f1bd2d37195868f4e53ebd76e8ea0
Richard <richard.yang.tong@gmail.com>
2021-06-24 15:23:16 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/SportConfig.java
@@ -1,8 +1,14 @@
-//package com.dragontalker.springdemo;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SportConfig {
-//	
-//}
+package com.dragontalker.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.dragontalker.springdemo")
+public class SportConfig {
+	
+	// define bean for our sad fortune service
+	
+	// define bean for our swim coach AND inject dependency
+	
+}
