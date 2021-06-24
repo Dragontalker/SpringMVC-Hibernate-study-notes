f049f679b806b5fb0a28c4d7e95997f64de1f68f
Richard <richard.yang.tong@gmail.com>
2021-06-24 10:32:42 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/AnnotationDemoApp.java
@@ -1,10 +1,14 @@
 package com.dragontalker.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationDemoApp {
 	
 	public static void main(String[] args) {
 		
 		// read spring config file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// get the bean from spring container
 		
