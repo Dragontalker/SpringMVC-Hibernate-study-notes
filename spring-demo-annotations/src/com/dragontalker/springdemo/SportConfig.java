9db62f47c95645accd0b0904c6170e9d64564a1f
Richard <richard.yang.tong@gmail.com>
2021-06-24 15:18:27 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/SportConfig.java
@@ -1,10 +1,8 @@
 //package com.dragontalker.springdemo;
 //
-//import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.context.annotation.Configuration;
 //
 //@Configuration
-//@ComponentScan("com.dragontalker.springdemo")
 //public class SportConfig {
 //	
 //}
