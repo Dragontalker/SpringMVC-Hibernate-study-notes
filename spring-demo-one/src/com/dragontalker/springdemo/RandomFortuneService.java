e8a78e63369af493b48ef0a44f3577c802710577
Richard <richard.yang.tong@gmail.com>
2021-06-24 09:11:42 -0400

    Added notes.

---
+++ spring-demo-one/src/com/dragontalker/springdemo/RandomFortuneService.java
@@ -0,0 +1,13 @@
+package com.dragontalker.springdemo;
+
+public class RandomFortuneService implements FortuneService {
+	
+
+	@Override
+	public String getFortune() {
+		String[] service = {"Who am I?", "Where am I from?", "Where should I go?"};
+		int index = (int) Math.random() * service.length;
+		return service[index];
+	}
+
+}
