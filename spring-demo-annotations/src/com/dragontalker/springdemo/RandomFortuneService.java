b3ea89451947b38f79e45ace6ce4df16b252968e
Richard <richard.yang.tong@gmail.com>
2021-06-24 12:02:12 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/RandomFortuneService.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/RandomFortuneService.java
@@ -4,10 +4,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
+	
+	// create an array of strings
+	private String[] data = {
+			"Beware of the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+			"The journey is the reward"
+	}
 
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
+		//pick a random string from the array
 		return null;
 	}
 
