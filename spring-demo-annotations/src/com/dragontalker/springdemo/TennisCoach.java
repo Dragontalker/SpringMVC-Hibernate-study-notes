b24a70182d1565ce51d05b7aaf3448af7d877c66
Richard <richard.yang.tong@gmail.com>
2021-06-24 11:44:10 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/TennisCoach.java
@@ -8,15 +8,6 @@ public class TennisCoach implements Coach {
 	
 	@Autowired
 	private FortuneService fortuneService;
-	
-	public TennisCoach() {
-		System.out.println(">> TennisCoach: inside default constructor");
-	}
-
-	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println(">> TennisCoach: inside setter method for fortuneService");
-		this.fortuneService = fortuneService;
-	}
 
 	@Override
 	public String getDailyWorkout() {
