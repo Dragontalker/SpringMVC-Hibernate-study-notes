9d5f6387f4cc82eb1140061eb6a1af441e0c2dcf
Richard <richard.yang.tong@gmail.com>
2021-06-24 11:53:00 -0400

    Added notes.

---
+++ spring-demo-annotations/src/com/dragontalker/springdemo/RandomFortuneService.java
@@ -0,0 +1,11 @@
+package com.dragontalker.springdemo;
+
+public class RandomFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
