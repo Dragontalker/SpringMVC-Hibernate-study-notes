2ac65bf208b32389f0bd19671ef84c1a2f6f5b2e
Richard <richard.yang.tong@gmail.com>
2021-06-24 12:02:50 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/RandomFortuneService.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/RandomFortuneService.java
@@ -1,5 +1,7 @@
 package com.dragontalker.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +12,10 @@ public class RandomFortuneService implements FortuneService {
 			"Beware of the wolf in sheep's clothing",
 			"Diligence is the mother of good luck",
 			"The journey is the reward"
-	}
+	};
+	
+	// create a random number generator
+	private Random myRandom = new Random();
 
 	@Override
 	public String getFortune() {
