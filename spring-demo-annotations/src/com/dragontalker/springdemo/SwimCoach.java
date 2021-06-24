d75f1ff53c071101fb2a49c5cc2c28a23a8b0b55
Richard <richard.yang.tong@gmail.com>
2021-06-24 15:48:52 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/SwimCoach.java
@@ -1,8 +1,16 @@
 package com.dragontalker.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService theFortuneService) {
 		this.fortuneService = theFortuneService;
