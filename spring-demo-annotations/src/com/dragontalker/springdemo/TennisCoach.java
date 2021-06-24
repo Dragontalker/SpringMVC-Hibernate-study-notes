bdf71a7f9ce06eeb36836bfeddf4546080c8f7bb
Richard <richard.yang.tong@gmail.com>
2021-06-24 11:18:37 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/TennisCoach.java
@@ -1,9 +1,17 @@
 package com.dragontalker.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
