60c50db0dffc25f052dc69873a19f34082a70418
Richard <richard.yang.tong@gmail.com>
2021-06-24 15:20:45 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/SwimCoach.java
@@ -1,6 +1,12 @@
 package com.dragontalker.springdemo;
 
 public class SwimCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	public SwimCoach(FortuneService theFortuneService) {
+		this.fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
