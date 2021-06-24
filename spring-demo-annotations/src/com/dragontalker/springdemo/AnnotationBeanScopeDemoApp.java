080f735756ec80b57a31c55cd861a517efcc8956
Richard <richard.yang.tong@gmail.com>
2021-06-24 12:18:26 -0400

    Added notes.

--- spring-demo-annotations/src/com/dragontalker/springdemo/AnnotationBeanScopeDemoApp.java
+++ spring-demo-annotations/src/com/dragontalker/springdemo/AnnotationBeanScopeDemoApp.java
@@ -24,6 +24,9 @@ public class AnnotationBeanScopeDemoApp {
 		System.out.println("\nMemory location for theCoach: " + theCoach);
 		
 		System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+		
+		// close the context
+		context.close();
 
 	}
 
