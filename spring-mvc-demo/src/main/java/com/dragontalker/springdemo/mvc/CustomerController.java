39c7ea2b3beb87d9e9bed1bbcd6dcb4ff348e847
Richard <richard.yang.tong@gmail.com>
2021-06-25 11:57:06 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/CustomerController.java
@@ -1,7 +1,11 @@
 package com.dragontalker.springdemo.mvc;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +21,9 @@ public class CustomerController {
 	}
 	
 	@RequestMapping("/processForm")
-	public String processForm() {
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer theCustomer,
+			BindingResult theBindingResult) {
 		
 		return "customer-confirmation";
 	}
