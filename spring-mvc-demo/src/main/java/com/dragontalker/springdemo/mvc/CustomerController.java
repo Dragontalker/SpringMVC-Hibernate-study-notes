c8cdc9339fe44803c4495185cb7b2f4db5ec1418
Richard <richard.yang.tong@gmail.com>
2021-06-25 14:01:28 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/CustomerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// add an initbinder ... to convert trim input strings
+	// add an initBinder ... to convert trim input strings
 	// remove leading and trailing whitespace
 	// resolve issue for our validation
 	
@@ -40,6 +40,10 @@ public class CustomerController {
 	public String processForm(
 			@Valid @ModelAttribute("customer") Customer theCustomer,
 			BindingResult theBindingResult) {
+		
+		System.out.println("Binding result: " + theBindingResult);
+		
+		System.out.println("\n\n\n\n");
 		
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
