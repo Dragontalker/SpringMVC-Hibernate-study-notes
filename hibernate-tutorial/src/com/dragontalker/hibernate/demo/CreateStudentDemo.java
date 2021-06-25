fbdaaa5bf84937b9fd0b6bd731e2f371194dc322
Richard <richard.yang.tong@gmail.com>
2021-06-25 16:32:07 -0400

    Added notes.

--- hibernate-tutorial/src/com/dragontalker/hibernate/demo/CreateStudentDemo.java
+++ hibernate-tutorial/src/com/dragontalker/hibernate/demo/CreateStudentDemo.java
@@ -1,9 +1,21 @@
 package com.dragontalker.hibernate.demo;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dragontalker.hibernate.demo.entity.Student;
+
 public class CreateStudentDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
 
 	}
 
