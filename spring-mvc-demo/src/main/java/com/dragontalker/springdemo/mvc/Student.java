9b7107a3f70af6858cf6bdb93f9073773c4c456e
Richard <richard.yang.tong@gmail.com>
2021-06-24 20:51:35 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/Student.java
@@ -8,4 +8,20 @@ public class Student {
 	
 	public Student() {}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 }
