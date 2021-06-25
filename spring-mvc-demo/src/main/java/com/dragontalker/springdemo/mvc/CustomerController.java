115a27d145276c8889a0db86391c31f5781e8ff5
Richard <richard.yang.tong@gmail.com>
2021-06-25 12:16:18 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/CustomerController.java
@@ -2,9 +2,12 @@ package com.dragontalker.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,6 +18,14 @@ public class CustomerController {
 	// add an initbinder ... to convert trim input strings
 	// remove leading and trailing whitespace
 	// resolve issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEdditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEdditor);
+	}
 	
 		
 	@RequestMapping("/showForm")
