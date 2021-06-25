f9767310ec92db8377070a24f8fcf8de7d672bf3
Richard <richard.yang.tong@gmail.com>
2021-06-24 20:53:47 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/StudentController.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/StudentController.java
@@ -1,10 +1,16 @@
 package com.dragontalker.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+	
+	public String showForm(Model theModel) {
+		
+		return "student-form";
+	}
 
 }
