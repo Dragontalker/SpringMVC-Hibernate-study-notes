1290e3c7ab2d7dc7fce989661738bf81558edf65
Richard <richard.yang.tong@gmail.com>
2021-06-25 15:03:23 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -17,7 +17,13 @@ public class CourseCodeConstraintValidator
 	public boolean isValid(String theCode, 
 			ConstraintValidatorContext theConstraintValidatorContext) {
 		
-		boolean result = theCode.startsWith(coursePrefix);
+		boolean result;
+		
+		if (theCode != null) {
+			result = theCode.startsWith(coursePrefix);
+		} else {
+			result = true;
+		}
 		
 		return result;
 	}
