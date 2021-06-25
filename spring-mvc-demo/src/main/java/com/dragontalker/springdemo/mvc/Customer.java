d676dacb8b5992303a1131de0105067543469c19
Richard <richard.yang.tong@gmail.com>
2021-06-25 13:34:35 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/Customer.java
@@ -19,7 +19,7 @@ public class Customer {
 	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="msut be less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@NotNull(message="is required")
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
@@ -41,11 +41,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
