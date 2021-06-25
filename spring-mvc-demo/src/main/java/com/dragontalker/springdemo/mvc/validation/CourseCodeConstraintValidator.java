37f9eb4014e4333f961b6d627cd305b301601fd8
Richard <richard.yang.tong@gmail.com>
2021-06-25 14:55:16 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -5,6 +5,13 @@ import javax.validation.ConstraintValidatorContext;
 
 public class CourseCodeConstraintValidator 
 	implements ConstraintValidator<CourceCode, String>{
+	
+	private String coursePrefix;
+	
+	@Override
+	public void initialize(CourceCode theCourseCode) {
+		coursePrefix = theCourseCode.value();
+	}
 
 	@Override
 	public boolean isValid(String arg0, ConstraintValidatorContext arg1) {
