2ea1572696881234034e651b672bb4e29dfef500
Richard <richard.yang.tong@gmail.com>
2021-06-25 14:16:15 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/validation/CourceCode.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/validation/CourceCode.java
@@ -1,8 +1,12 @@
 package com.dragontalker.springdemo.mvc.validation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 import javax.validation.Constraint;
 
 @Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD } )
 public @interface CourceCode {
 
 }
