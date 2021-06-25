3e3e424c6e694db066b520e9cbf6b38a3abbd13c
Richard <richard.yang.tong@gmail.com>
2021-06-25 13:27:07 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/Customer.java
@@ -3,6 +3,7 @@ package com.dragontalker.springdemo.mvc;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
@@ -16,6 +17,9 @@ public class Customer {
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="msut be less than or equal to 10")
 	private int freePasses;
+	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
 
 	public String getFirstName() {
 		return firstName;
