e7fbc781d9b1afb8e2370558aaf78ae117bf85e8
Richard <richard.yang.tong@gmail.com>
2021-06-25 16:20:27 -0400

    Added notes.

--- hibernate-tutorial/src/com/dragontalker/hibernate/demo/entity/Student.java
+++ hibernate-tutorial/src/com/dragontalker/hibernate/demo/entity/Student.java
@@ -23,4 +23,13 @@ public class Student {
 	private String email;
 
 	public Student() {}
+
+	public Student(int id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
 }
