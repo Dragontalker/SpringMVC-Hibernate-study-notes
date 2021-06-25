626112c42cac13291b06b207bd4a4666f8d4bcc7
Richard <richard.yang.tong@gmail.com>
2021-06-25 11:41:39 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/Customer.java
@@ -1,5 +1,25 @@
 package com.dragontalker.springdemo.mvc;
 
 public class Customer {
+	
+	private String firstName;
+	
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 }
