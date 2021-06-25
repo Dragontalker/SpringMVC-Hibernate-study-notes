4ff76a5a92b5b9faf13db7106b8fbeffe3ba3647
Richard <richard.yang.tong@gmail.com>
2021-06-25 14:54:09 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/validation/CourseCodeConstraintValidator.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -1,5 +1,15 @@
 package com.dragontalker.springdemo.mvc.validation;
 
-public class CourseCodeConstraintValidator {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator 
+	implements ConstraintValidator<CourceCode, String>{
+
+	@Override
+	public boolean isValid(String arg0, ConstraintValidatorContext arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
