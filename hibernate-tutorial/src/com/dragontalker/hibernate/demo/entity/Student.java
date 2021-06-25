50aea3371969d2a5ede0b2c83ae6428089cd5029
Richard <richard.yang.tong@gmail.com>
2021-06-25 16:19:17 -0400

    Added notes.

--- hibernate-tutorial/src/com/dragontalker/hibernate/demo/entity/Student.java
+++ hibernate-tutorial/src/com/dragontalker/hibernate/demo/entity/Student.java
@@ -1,12 +1,16 @@
 package com.dragontalker.hibernate.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="student")
 public class Student {
 	
+	@Id
+	@Column(name="id")
 	private int id;
 	
 	private String firstName;
