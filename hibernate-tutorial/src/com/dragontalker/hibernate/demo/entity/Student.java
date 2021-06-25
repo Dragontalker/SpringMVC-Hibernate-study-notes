1e5836f0afdf160fad54f0fa1ee86a28b73a229b
Richard <richard.yang.tong@gmail.com>
2021-06-25 16:18:50 -0400

    Added notes.

--- hibernate-tutorial/src/com/dragontalker/hibernate/demo/entity/Student.java
+++ hibernate-tutorial/src/com/dragontalker/hibernate/demo/entity/Student.java
@@ -6,5 +6,14 @@ import javax.persistence.Table;
 @Entity
 @Table(name="student")
 public class Student {
+	
+	private int id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
 
+	public Student() {}
 }
