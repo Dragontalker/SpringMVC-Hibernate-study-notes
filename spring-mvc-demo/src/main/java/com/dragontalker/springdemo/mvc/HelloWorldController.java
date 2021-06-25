e564a1c9630874fa0d13dfb758a75b6576898b52
Richard <richard.yang.tong@gmail.com>
2021-06-24 20:10:13 -0400

    Added notes.

--- spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/main/java/com/dragontalker/springdemo/mvc/HelloWorldController.java
@@ -30,13 +30,16 @@ public class HelloWorldController {
 	public String letsShoutDude(HttpServletRequest request, Model model) {
 		 
 		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
 		
 		// convert the data to all caps
+		theName = theName.toUpperCase();
 		
 		// create the message
+		String result = "Yo! " + theName;
 		
 		// add message to the model
-	
+		model.addAttribute("message", result);
 		
 		return "helloworld";
 	}
