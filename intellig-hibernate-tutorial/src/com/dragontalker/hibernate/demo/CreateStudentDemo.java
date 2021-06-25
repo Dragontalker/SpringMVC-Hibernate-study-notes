3fdf5e99918f451003c228b24143fe3cc69f7b3c
Richard <richard.yang.tong@gmail.com>
2021-06-25 17:46:20 -0400

    Added notes.

--- intellig-hibernate-tutorial/src/com/dragontalker/hibernate/demo/CreateStudentDemo.java
+++ intellig-hibernate-tutorial/src/com/dragontalker/hibernate/demo/CreateStudentDemo.java
@@ -1,6 +1,7 @@
 package com.dragontalker.hibernate.demo;
 
 import com.dragontalker.hibernate.demo.entity.Student;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -13,5 +14,14 @@ public class CreateStudentDemo {
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Student.class)
                 .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // use the session object to save Java object
+        } finally {
+            factory.close();
+        }
     }
 }
