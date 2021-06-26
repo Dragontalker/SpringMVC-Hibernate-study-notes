31e3f8c38935d1e4f04829133eaafe3a9aacea2a
Richard <richard.yang.tong@gmail.com>
2021-06-26 10:58:01 -0400

    Added notes.

--- hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/InstructorDetail.java
+++ hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/InstructorDetail.java
@@ -1,5 +1,17 @@
 package com.dragontalker.hibernate.demo.entity;
 
 public class InstructorDetail {
+	
+	// annotate the class as an entity and map to db table
+	
+	// define the fields
+	
+	// annotate the fields with db column naes
+	
+	// create constructors
+	
+	// generate getter/setter methods
+	
+	// generate toString() method
 
 }
