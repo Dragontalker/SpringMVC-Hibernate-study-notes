97d8f81e006e8a9e9c160d07d7d220c949ec1b03
Richard <richard.yang.tong@gmail.com>
2021-06-26 12:14:04 -0400

    Added notes.

---
+++ hb-02-one-to-one-bi/src/com/dragontalker/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,44 @@
+package com.dragontalker.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dragontalker.hibernate.demo.entity.Instructor;
+import com.dragontalker.hibernate.demo.entity.InstructorDetail;
+
+
+public class GetInstructorDetailDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+        	// start a transaction
+            session.beginTransaction();
+
+            // get the instructor detail object
+            
+            // print the instructor detail
+            
+            // print the associated instructor
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!!!");
+
+        } finally {
+            factory.close();
+        }
+    }
+}
