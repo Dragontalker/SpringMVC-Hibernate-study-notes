a8aa93f142545f1d87197459fa5b74d529cb5345
Richard <richard.yang.tong@gmail.com>
2021-06-26 11:04:31 -0400

    Added notes.

--- hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/Instructor.java
+++ hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/Instructor.java
@@ -6,5 +6,13 @@ import javax.persistence.Table;
 @Entity
 @Table(name="instructor")
 public class Instructor {
+	
+	private int id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
 
 }
