ec0b6af4ce643cdb1074ad748170f6ac00dda278
Richard <richard.yang.tong@gmail.com>
2021-06-26 10:58:40 -0400

    Added notes.

--- hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/InstructorDetail.java
+++ hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/InstructorDetail.java
@@ -1,5 +1,10 @@
 package com.dragontalker.hibernate.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor_detial")
 public class InstructorDetail {
 	
 	// annotate the class as an entity and map to db table
