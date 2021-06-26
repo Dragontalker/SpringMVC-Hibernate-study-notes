fbb57b4de719b28c352908f2bef8f446de384f36
Richard <richard.yang.tong@gmail.com>
2021-06-26 11:15:48 -0400

    Added notes.

--- hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/Instructor.java
+++ hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/Instructor.java
@@ -32,4 +32,12 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
+	public Instructor() {}
+
+	public Instructor(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
 }
