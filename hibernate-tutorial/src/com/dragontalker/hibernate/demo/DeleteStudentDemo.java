445d3df8d5d1d2d95d8e5660727c2cf93df20bae
Richard <richard.yang.tong@gmail.com>
2021-06-26 09:44:57 -0400

    Added notes.

--- hibernate-tutorial/src/com/dragontalker/hibernate/demo/DeleteStudentDemo.java
+++ hibernate-tutorial/src/com/dragontalker/hibernate/demo/DeleteStudentDemo.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class UpdateStudentDemo {
+public class DeleteStudentDemo {
 
     public static void main(String[] args) {
 
@@ -28,6 +28,22 @@ public class UpdateStudentDemo {
             System.out.println("\nGetting student with id: " + studentId);
             
             Student myStudent = session.get(Student.class, studentId);
+            
+            System.out.println("Updating student...");
+            myStudent.setFirstName("Scooby");
+            
+            // commit the transaction
+            session.getTransaction().commit();
+            
+            // NEW CODE
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            
+            // update email for all students
+            System.out.println("Update email for all students");
+            
+            session.createQuery("update Student set email='foo@gmail.com'")
+            	.executeUpdate();
             
             // commit the transaction
             session.getTransaction().commit();
