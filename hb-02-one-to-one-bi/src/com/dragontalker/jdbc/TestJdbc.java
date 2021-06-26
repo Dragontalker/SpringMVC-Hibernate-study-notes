a799cbe2520a79ff44a84ddae21535dcf4c81e7e
Richard <richard.yang.tong@gmail.com>
2021-06-26 12:05:44 -0400

    Added notes.

---
+++ hb-02-one-to-one-bi/src/com/dragontalker/jdbc/TestJdbc.java
@@ -0,0 +1,27 @@
+package com.dragontalker.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
+        String user = "hbstudent";
+        String pass = "hbstudent";
+
+        try {
+            System.out.println("Connecting to database: " + jdbcUrl);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            System.out.println("Connection successful!!!");
+            System.out.println("Connection confirmed: " + myConn);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
