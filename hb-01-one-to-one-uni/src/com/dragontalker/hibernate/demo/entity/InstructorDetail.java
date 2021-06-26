ff2c7ec53ff8d3e3b33a64703ac02b63ec7c2aa1
Richard <richard.yang.tong@gmail.com>
2021-06-26 11:01:54 -0400

    Added notes.

--- hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/InstructorDetail.java
+++ hb-01-one-to-one-uni/src/com/dragontalker/hibernate/demo/entity/InstructorDetail.java
@@ -11,18 +11,6 @@ import javax.persistence.Table;
 @Table(name="instructor_detial")
 public class InstructorDetail {
 	
-	// annotate the class as an entity and map to db table
-	
-	// define the fields
-	
-	// annotate the fields with db column naes
-	
-	// create constructors
-	
-	// generate getter/setter methods
-	
-	// generate toString() method
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
