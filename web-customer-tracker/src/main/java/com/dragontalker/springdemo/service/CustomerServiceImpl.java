80981535b9e0831c3ca9fb0e599c4c360b76dc47
Richard <richard.yang.tong@gmail.com>
2021-06-27 19:27:52 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/service/CustomerServiceImpl.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/service/CustomerServiceImpl.java
@@ -2,10 +2,19 @@ package com.dragontalker.springdemo.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dragontalker.springdemo.dao.CustomerDAO;
 import com.dragontalker.springdemo.entity.Customer;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
+	// need to inject customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@Override
 	public List<Customer> getCustomers() {
 		// TODO Auto-generated method stub
