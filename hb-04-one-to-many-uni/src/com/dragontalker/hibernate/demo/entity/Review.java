25822fdc37758863b76092b5a193df3ea95c1571
Richard <richard.yang.tong@gmail.com>
2021-06-27 09:26:52 -0400

    Added notes.

--- hb-04-one-to-many-uni/src/com/dragontalker/hibernate/demo/entity/Review.java
+++ hb-04-one-to-many-uni/src/com/dragontalker/hibernate/demo/entity/Review.java
@@ -1,14 +1,22 @@
 package com.dragontalker.hibernate.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "review")
 public class Review {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
 	
+	@Column(name = "comment")
 	private String comment;
 	
 	public Review() {}
