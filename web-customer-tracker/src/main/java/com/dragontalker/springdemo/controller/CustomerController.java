54da2c41eb819344591f56b0dec9b06c653a6edd
Richard <richard.yang.tong@gmail.com>
2021-06-27 16:48:07 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package com.dragontalker.springdemo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dragontalker.springdemo.dao.CustomerDAO;
+import com.dragontalker.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
@@ -19,9 +22,10 @@ public class CustomerController {
 	public String listCustomers(Model theModel) {
 		
 		// get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
 		
 		// add the customers to the model
-		
+		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
 	}
