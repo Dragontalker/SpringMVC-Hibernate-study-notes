c6154f1c40dfaabfaf258eeb1328e67abc8c276b
Richard <richard.yang.tong@gmail.com>
2021-06-26 20:12:18 -0400

    Added notes.

--- hb-03-one-to-many/src/com/dragontalker/hibernate/demo/entity/Course.java
+++ hb-03-one-to-many/src/com/dragontalker/hibernate/demo/entity/Course.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -19,6 +21,8 @@ public class Course {
 	@Column(name = "title")
 	private String title;
 	
+	@ManyToOne
+	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
 	
 	public Course() {}
