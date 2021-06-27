bb97f47b02dd7e032c359c71970fa30a112ba059
Richard <richard.yang.tong@gmail.com>
2021-06-26 20:33:55 -0400

    Added notes.

--- hb-03-one-to-many/src/com/dragontalker/hibernate/demo/CreateInstructorDemo.java
+++ hb-03-one-to-many/src/com/dragontalker/hibernate/demo/CreateInstructorDemo.java
@@ -25,29 +25,10 @@ public class CreateInstructorDemo {
         Session session = factory.getCurrentSession();
 
         try {
-        	
-        	// create the objects
-        	Instructor tempInstructor = 
-        			new Instructor("Susan", "Public", "susan.public@luv2code.com");
-        	
-        	InstructorDetail tempInstructorDetail =
-        			new InstructorDetail(
-        					"http://www.youtube.com",
-        					"Video Games");
-        	
-        	// associate the objects
-        	tempInstructor.setInstructorDetail(tempInstructorDetail);
-        	
         	// start a transaction
             session.beginTransaction();
             
-        	// save the instructor
-        	//
-        	// Note: this will ALSO save the details object
-        	// because of CascadeType.ALL
-        	//
-        	System.out.println("Saving instructor: " + tempInstructor);
-        	session.save(tempInstructor);
+
 
             // commit the transaction
             session.getTransaction().commit();
