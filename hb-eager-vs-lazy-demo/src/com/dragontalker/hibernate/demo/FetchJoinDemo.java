5b9bed3f673cf4c19f4a14cb1121456a9bd86d7d
Richard <richard.yang.tong@gmail.com>
2021-06-26 22:19:35 -0400

    Added notes.

---
+++ hb-eager-vs-lazy-demo/src/com/dragontalker/hibernate/demo/FetchJoinDemo.java
@@ -0,0 +1,62 @@
+package com.dragontalker.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dragontalker.hibernate.demo.entity.Course;
+import com.dragontalker.hibernate.demo.entity.Instructor;
+import com.dragontalker.hibernate.demo.entity.InstructorDetail;
+
+
+public class FetchJoinDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+        	// start a transaction
+            session.beginTransaction();
+            
+            // get the instructor from db
+            int theId = 1;
+            Instructor tempInstructor = session.get(Instructor.class, theId);
+            
+            System.out.println(">> dragontalker: Instructor: " + tempInstructor);
+
+            // option 1: call getter method while session is open
+            System.out.println(">> Before session.close(): Courses: " + tempInstructor.getCourses());
+            
+            // commit the transaction
+            session.getTransaction().commit();
+            
+            // close the session
+            session.close();
+            
+            System.err.println("\n>> Session is now closed!\n");
+            
+            // get courses for the instructor
+            System.out.println(">> After session.close(): Courses: " + tempInstructor.getCourses());
+
+            System.out.println(">> dragontalker: Done!!!");
+
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+        	// add clean up code
+        	session.close();
+        	
+            factory.close();
+        }
+    }
+}
