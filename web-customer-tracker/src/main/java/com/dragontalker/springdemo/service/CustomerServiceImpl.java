9559aa6bbb72a1bd98b72355d0020d74d248ec84
Richard <richard.yang.tong@gmail.com>
2021-06-27 19:26:20 -0400

    Added notes.

---
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,15 @@
+package com.dragontalker.springdemo.service;
+
+import java.util.List;
+
+import com.dragontalker.springdemo.entity.Customer;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public List<Customer> getCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
