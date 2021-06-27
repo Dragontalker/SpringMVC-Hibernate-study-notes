f3f25938895cdb979f78ab62040ba94dd4b4e891
Richard <richard.yang.tong@gmail.com>
2021-06-27 16:32:08 -0400

    Added notes.

---
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,15 @@
+package com.dragontalker.springdemo.dao;
+
+import java.util.List;
+
+import com.dragontalker.springdemo.entity.Customer;
+
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Override
+	public List<Customer> getCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
