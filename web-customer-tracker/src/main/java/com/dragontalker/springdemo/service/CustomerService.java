8125e29a1bad4e5abc06d840f5faad7abc266482
Richard <richard.yang.tong@gmail.com>
2021-06-27 19:25:46 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/service/CustomerService.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/service/CustomerService.java
@@ -1,5 +1,10 @@
 package com.dragontalker.springdemo.service;
 
+import java.util.List;
+
+import com.dragontalker.springdemo.entity.Customer;
+
 public interface CustomerService {
 
+	public List<Customer> getCustomers();
 }
