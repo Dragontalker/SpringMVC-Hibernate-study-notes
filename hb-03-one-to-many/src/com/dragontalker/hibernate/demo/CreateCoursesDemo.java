fe23e80a99ab179bc6ffef48c82f8a5eb6c2824d
Richard <richard.yang.tong@gmail.com>
2021-06-26 20:32:16 -0400

    Added notes.

--- hb-03-one-to-many/src/com/dragontalker/hibernate/demo/CreateCoursesDemo.java
+++ hb-03-one-to-many/src/com/dragontalker/hibernate/demo/CreateCoursesDemo.java
@@ -29,12 +29,20 @@ public class CreateCoursesDemo {
             session.beginTransaction();
             
             // get the instructor from db
+            int theId = 1;
+            Instructor tempInstructor = session.get(Instructor.class, theId);
             
             // create some courses
+            Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
+            Course tempCourse2 = new Course("The Pinball Masterclass");
             
             // add courses to instructor
+            tempInstructor.add(tempCourse1);
+            tempInstructor.add(tempCourse2);
             
             // save the courses
+            session.save(tempCourse1);
+            session.save(tempCourse2);
 
             // commit the transaction
             session.getTransaction().commit();
