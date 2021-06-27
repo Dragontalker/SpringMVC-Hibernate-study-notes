2de2d3c103a5607118b35a37ea479f418f960f61
Richard <richard.yang.tong@gmail.com>
2021-06-27 09:32:03 -0400

    Added notes.

--- hb-04-one-to-many-uni/src/com/dragontalker/hibernate/demo/entity/Course.java
+++ hb-04-one-to-many-uni/src/com/dragontalker/hibernate/demo/entity/Course.java
@@ -33,6 +33,7 @@ public class Course {
 	private Instructor instructor;
 
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
 	private List<Review> reviews;
 	
 	public Course() {}
