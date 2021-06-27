93a5c3b2c5a0483f8433dc6d2a07f45d659e5328
Richard <richard.yang.tong@gmail.com>
2021-06-27 16:32:51 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/dao/CustomerDAOImpl.java
@@ -2,9 +2,16 @@ package com.dragontalker.springdemo.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.dragontalker.springdemo.entity.Customer;
+import com.mysql.cj.xdevapi.SessionFactory;
 
 public class CustomerDAOImpl implements CustomerDAO {
+	
+	// need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	@Override
 	public List<Customer> getCustomers() {
