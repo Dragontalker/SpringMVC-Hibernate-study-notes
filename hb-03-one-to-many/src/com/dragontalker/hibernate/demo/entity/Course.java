01bbe6de93f39a988c6f842e53e32c4f89efd2be
Richard <richard.yang.tong@gmail.com>
2021-06-26 20:11:42 -0400

    Added notes.

--- hb-03-one-to-many/src/com/dragontalker/hibernate/demo/entity/Course.java
+++ hb-03-one-to-many/src/com/dragontalker/hibernate/demo/entity/Course.java
@@ -1,14 +1,22 @@
 package com.dragontalker.hibernate.demo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "course")
 public class Course {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
 	
+	@Column(name = "title")
 	private String title;
 	
 	private Instructor instructor;
