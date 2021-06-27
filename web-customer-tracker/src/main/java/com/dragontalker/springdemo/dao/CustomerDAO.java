3ee5c55e76f8129889ca57a9e907a0d4ee307379
Richard <richard.yang.tong@gmail.com>
2021-06-27 16:30:46 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/dao/CustomerDAO.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/dao/CustomerDAO.java
@@ -1,5 +1,10 @@
 package com.dragontalker.springdemo.dao;
 
+import java.util.List;
+
+import com.dragontalker.springdemo.entity.Customer;
+
 public interface CustomerDAO {
 
+	public List<Customer> getCustomers();
 }
