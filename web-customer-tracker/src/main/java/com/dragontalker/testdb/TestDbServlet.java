90b75f92147813b91bb2244e00dc75c8c81712a4
Richard <richard.yang.tong@gmail.com>
2021-06-27 15:20:24 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/testdb/TestDbServlet.java
+++ web-customer-tracker/src/main/java/com/dragontalker/testdb/TestDbServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.sql.*;
+
 /**
  * Servlet implementation class TestDbServlet
  */
