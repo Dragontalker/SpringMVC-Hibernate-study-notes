297dbed09be393650b23f9eb5cd680143dc1df36
Richard <richard.yang.tong@gmail.com>
2021-06-27 09:41:27 -0400

    Added notes.

---
+++ hb-04-one-to-many-uni/src/com/dragontalker/hibernate/demo/GetCourseAndReviewsDemo.java
@@ -0,0 +1,49 @@
+package com.dragontalker.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.dragontalker.hibernate.demo.entity.Course;
+import com.dragontalker.hibernate.demo.entity.Instructor;
+import com.dragontalker.hibernate.demo.entity.InstructorDetail;
+import com.dragontalker.hibernate.demo.entity.Review;
+
+
+public class GetCourseAndReviewsDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+        	// start a transaction
+            session.beginTransaction();
+
+            
+            
+            // commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println(">> Done!!!");
+
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+        	// add clean up code
+        	session.close();
+        	
+            factory.close();
+        }
+    }
+}
