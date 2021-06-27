90dd7e5ffba88a5781f133dad8424f354626458f
Richard <richard.yang.tong@gmail.com>
2021-06-27 16:16:39 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/entity/Customer.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/entity/Customer.java
@@ -7,4 +7,11 @@ import javax.persistence.Table;
 @Table(name = "customer")
 public class Customer {
 
+	private int id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
 }
