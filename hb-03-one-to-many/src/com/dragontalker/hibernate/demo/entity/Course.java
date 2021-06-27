ec377c7d59ad60243b25924dbb7f52ff75d2af5d
Richard <richard.yang.tong@gmail.com>
2021-06-26 20:08:11 -0400

    Added notes.

--- hb-03-one-to-many/src/com/dragontalker/hibernate/demo/entity/Course.java
+++ hb-03-one-to-many/src/com/dragontalker/hibernate/demo/entity/Course.java
@@ -19,4 +19,28 @@ public class Course {
 		this.title = title;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
 }
