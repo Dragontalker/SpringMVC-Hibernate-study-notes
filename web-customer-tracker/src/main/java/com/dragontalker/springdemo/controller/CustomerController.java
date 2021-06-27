29d7b536a0c592ff3f51443065a628b2719af24e
Richard <richard.yang.tong@gmail.com>
2021-06-27 15:48:57 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/controller/CustomerController.java
@@ -1,5 +1,10 @@
 package com.dragontalker.springdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
 public class CustomerController {
 
 }
