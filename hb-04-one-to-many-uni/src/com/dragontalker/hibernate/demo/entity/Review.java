14468ad39f9eae202ef7e96ed42828c05ddca2b4
Richard <richard.yang.tong@gmail.com>
2021-06-27 09:24:54 -0400

    Added notes.

--- hb-04-one-to-many-uni/src/com/dragontalker/hibernate/demo/entity/Review.java
+++ hb-04-one-to-many-uni/src/com/dragontalker/hibernate/demo/entity/Review.java
@@ -18,4 +18,20 @@ public class Review {
 		this.comment = comment;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
 }
