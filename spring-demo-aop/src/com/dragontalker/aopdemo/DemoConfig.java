3434f9620f2811fda33fb825f678c799a82b4269
Richard <richard.yang.tong@gmail.com>
2021-06-28 09:31:10 -0400

    Added notes.

--- spring-demo-aop/src/com/dragontalker/aopdemo/DemoConfig.java
+++ spring-demo-aop/src/com/dragontalker/aopdemo/DemoConfig.java
@@ -1,5 +1,12 @@
 package com.dragontalker.aopdemo;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.dragontalker.aopdemo")
 public class DemoConfig {
 
 }
