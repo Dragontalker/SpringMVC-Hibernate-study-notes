f5a54879f3e137f2ec498129282d8e06db87815a
Richard <richard.yang.tong@gmail.com>
2021-06-27 20:02:07 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dragontalker.springdemo.entity.Customer;
@@ -40,6 +42,12 @@ public class CustomerController {
 		theModel.addAttribute("customer", theCustomer);
 		
 		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		return "redirect:/customer/list";
 	}
 	
 }
