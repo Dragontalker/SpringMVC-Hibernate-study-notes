3791f903fc7c1eb6761c28fed559f33493c64d3d
Richard <richard.yang.tong@gmail.com>
2021-06-28 12:12:08 -0400

    Added notes.

--- spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,21 +2,16 @@ package com.dragontalker.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 	
-	@Before("forDaoPackageNoGetterSetter()")
+	@Before("com.dragontalker.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddMethod() {
 		
 		System.out.println("\n=====>> Excuting @Before advice on any add method!");
 	}
-	
-	
-	
-	
 	
 }
