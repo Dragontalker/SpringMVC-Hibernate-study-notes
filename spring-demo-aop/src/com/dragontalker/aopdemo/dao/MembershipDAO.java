68cc30417ffa958556b785428eb22158544075f4
Richard <richard.yang.tong@gmail.com>
2021-06-28 10:08:54 -0400

    Added notes.

--- spring-demo-aop/src/com/dragontalker/aopdemo/dao/MembershipDAO.java
+++ spring-demo-aop/src/com/dragontalker/aopdemo/dao/MembershipDAO.java
@@ -1,5 +1,12 @@
 package com.dragontalker.aopdemo.dao;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MembershipDAO {
 
+	public void addAccount() {
+		
+		System.out.println(">> " + getClass() + ": DOING STUFF: ADDING A MEMBERSHIP ACCOUNT");
+	}
 }
