496f3595d9fa105d9bd10482a2a88c4888465962
Richard <richard.yang.tong@gmail.com>
2021-06-28 11:33:57 -0400

    Added notes.

--- spring-demo-aop-pointcut-declaration-combo/src/com/dragontalker/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-pointcut-declaration-combo/src/com/dragontalker/aopdemo/dao/AccountDAO.java
@@ -7,6 +7,25 @@ import com.dragontalker.aopdemo.Account;
 @Component
 public class AccountDAO {
 	
+	private String name;
+	private String serviceCode;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		this.serviceCode = serviceCode;
+	}
+
 	public void addAccount(Account theAccount, boolean vipFlag) {
 		
 		System.out.println(">> " + getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
