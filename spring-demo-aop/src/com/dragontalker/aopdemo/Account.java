0252fc4e4eafd8b894dd01485b6541e25de15549
Richard <richard.yang.tong@gmail.com>
2021-06-28 10:48:16 -0400

    Added notes.

--- spring-demo-aop/src/com/dragontalker/aopdemo/Account.java
+++ spring-demo-aop/src/com/dragontalker/aopdemo/Account.java
@@ -8,12 +8,15 @@ public class Account {
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getLevel() {
 		return level;
 	}
+	
 	public void setLevel(String level) {
 		this.level = level;
 	}
