c1221478f0deb07f790d9b1fd262d6fb847891fe
Richard <richard.yang.tong@gmail.com>
2021-06-28 09:47:21 -0400

    Added notes.

--- spring-demo-aop/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,6 +1,7 @@
 package com.dragontalker.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,4 +11,9 @@ public class MyDemoLoggingAspect {
 	// this is where we add all of our related advices for logging
 	
 	// let's start with an @Before advice
+	@Before("execution(public void addAccount())")
+	public void beforeAddAccountAdvice() {
+		
+		System.out.println("\n>> Excuting @Before advice on addAccount()");
+	}
 }
