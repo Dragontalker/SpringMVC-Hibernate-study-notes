8e0c43866fc3ce3db2fdff13cf99d8b316e95509
Richard <richard.yang.tong@gmail.com>
2021-06-28 13:55:13 -0400

    Added notes.

--- spring-demo-aop-z-after-returning/src/com/dragontalker/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-z-after-returning/src/com/dragontalker/aopdemo/dao/AccountDAO.java
@@ -18,6 +18,16 @@ public class AccountDAO {
 		
 		List<Account> myAccounts = new ArrayList<>();
 		
+		// create sample accounts
+		Account temp1 = new Account("John", "Silver");
+		Account temp2 = new Account("Madhu", "Platinum");
+		Account temp3 = new Account("Luca", "Gold");
+		
+		// add them to our accounts list
+		myAccounts.add(temp1);
+		myAccounts.add(temp2);
+		myAccounts.add(temp3);
+		
 		return myAccounts;
 	}
 	
