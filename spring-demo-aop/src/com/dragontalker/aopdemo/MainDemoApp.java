6f2fdea37d2abc9c4de3c747bb9dee8331271daf
Richard <richard.yang.tong@gmail.com>
2021-06-28 09:39:09 -0400

    Added notes.

--- spring-demo-aop/src/com/dragontalker/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/dragontalker/aopdemo/MainDemoApp.java
@@ -1,14 +1,18 @@
 package com.dragontalker.aopdemo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class MainDemoApp {
 
 	public static void main(String[] args) {
 		
-		// read spring config java class
+		// read spring configuration java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
 		
 		// get the bean from spring container
 		
-		// call the bussiness method
+		// call the business method
 		
 		// close the context
 		
