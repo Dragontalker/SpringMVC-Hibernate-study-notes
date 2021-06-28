c5f59c867dbfede00a2b322c2e9a7704b6e2501b
Richard <richard.yang.tong@gmail.com>
2021-06-28 17:28:56 -0400

    Added notes.

--- web-customer-tracker-aop/src/main/java/com/dragontalker/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/main/java/com/dragontalker/springdemo/aspect/CRMLoggingAspect.java
@@ -1,5 +1,10 @@
 package com.dragontalker.springdemo.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class CRMLoggingAspect {
 
 }
