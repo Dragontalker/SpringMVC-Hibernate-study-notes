2d859438930266e9330c72e249a7dedb5bb96df9
Richard <richard.yang.tong@gmail.com>
2021-06-28 12:06:51 -0400

    Added notes.

--- spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/LuvAopExpressions.java
+++ spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/LuvAopExpressions.java
@@ -1,5 +1,10 @@
 package com.dragontalker.aopdemo.aspect;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LuvAopExpressions {
 
 }
