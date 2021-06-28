a38a8e4f94c8a77a2c58982f2bcad3e19d341294
Richard <richard.yang.tong@gmail.com>
2021-06-28 10:18:04 -0400

    Added notes.

--- spring-demo-aop/src/com/dragontalker/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop/src/com/dragontalker/aopdemo/dao/AccountDAO.java
@@ -9,4 +9,9 @@ public class AccountDAO {
 		
 		System.out.println(">> " + getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
 	}
+	
+	public void addSillyMember() {
+		
+		System.out.println(">> " + getClass() + ": DOING STUFF: ADDING A MEMBER");
+	}
 }
