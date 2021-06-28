9d0dea3689a1d74152b7358004e465b651e4d77e
Richard <richard.yang.tong@gmail.com>
2021-06-28 11:35:24 -0400

    Added notes.

--- spring-demo-aop-pointcut-declaration-combo/src/com/dragontalker/aopdemo/dao/AccountDAO.java
+++ spring-demo-aop-pointcut-declaration-combo/src/com/dragontalker/aopdemo/dao/AccountDAO.java
@@ -11,18 +11,22 @@ public class AccountDAO {
 	private String serviceCode;
 	
 	public String getName() {
+		System.out.println(">> " + getClass() + ": in getName()");
 		return name;
 	}
 
 	public void setName(String name) {
+		System.out.println(">> " + getClass() + ": in setName()");
 		this.name = name;
 	}
 
 	public String getServiceCode() {
+		System.out.println(">> " + getClass() + ": in getServiceCode()");
 		return serviceCode;
 	}
 
 	public void setServiceCode(String serviceCode) {
+		System.out.println(">> " + getClass() + ": in setServiceCode()");
 		this.serviceCode = serviceCode;
 	}
 
