ebdaca6ffcbfc3f1cbebded82388d4d3a445d08e
Richard <richard.yang.tong@gmail.com>
2021-06-27 21:55:58 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/dao/CustomerDAOImpl.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/dao/CustomerDAOImpl.java
@@ -43,7 +43,7 @@ public class CustomerDAOImpl implements CustomerDAO {
 		Session currentSession = sessionFactory.getCurrentSession();
 		
 		// save the customer ... finally LOL
-		currentSession.save(theCustomer);
+		currentSession.saveOrUpdate(theCustomer);
 		
 	}
 
