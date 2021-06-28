dd8a52658375113c6da2ec8101d8dc787cbe73b8
Richard <richard.yang.tong@gmail.com>
2021-06-28 11:32:54 -0400

    Added notes.

---
+++ spring-demo-aop-pointcut-declaration-combo/src/com/dragontalker/aopdemo/MainDemoApp.java
@@ -0,0 +1,37 @@
+package com.dragontalker.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.dragontalker.aopdemo.dao.AccountDAO;
+import com.dragontalker.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring configuration java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = 
+				context.getBean("accountDAO", AccountDAO.class);
+		
+		// get membership bean from spring container
+		MembershipDAO theMembershipDAO = 
+				context.getBean("membershipDAO", MembershipDAO.class);
+		
+		// call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		
+		
+		// call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+		
+		// close the context
+		context.close();
+	}
+}
---
+++ spring-demo-aop-pointcut-declaration-combo/src/com/dragontalker/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,20 @@
+package com.dragontalker.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.dragontalker.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+	
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		
+		System.out.println(">> " + getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println(">> " + getClass() + ": doWork()");
+		return false;
+	}
+}
