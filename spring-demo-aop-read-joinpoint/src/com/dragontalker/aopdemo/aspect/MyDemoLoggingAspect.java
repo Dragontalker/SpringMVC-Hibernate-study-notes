0e7ccb19b3f8d9fe5728571d9f348f5360db5d4f
Richard <richard.yang.tong@gmail.com>
2021-06-28 13:25:35 -0400

    Added notes.

--- spring-demo-aop-read-joinpoint/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-read-joinpoint/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,9 @@
 package com.dragontalker.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +13,17 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 	
 	@Before("com.dragontalker.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforeAddMethod() {
+	public void beforeAddMethod(JoinPoint theJoinPoint) {
 		
 		System.out.println("\n=====>> Excuting @Before advice on any add method!");
 		
 		// display the method signature
+		MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println(">> Method: " + methodSignature);
 		
 		// display method arguments
+		
 		
 	}
 	
