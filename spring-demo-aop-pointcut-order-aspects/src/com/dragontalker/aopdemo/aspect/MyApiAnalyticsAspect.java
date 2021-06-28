3207cb83b0803de1efe427ef6595ab23950cfd01
Richard <richard.yang.tong@gmail.com>
2021-06-28 12:08:58 -0400

    Added notes.

--- spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -1,10 +1,16 @@
 package com.dragontalker.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyApiAnalyticsAspect {
 
+	@Before("forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+		
+		System.out.println("\n=====>> Performing API analytics");
+	}
 }
--- spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,21 +8,6 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-
-	@Pointcut("execution(* com.dragontalker.aopdemo.dao.*.*(..))")
-	private void forDaoPackage() {}
-	
-	// create point cut for getter methods
-	@Pointcut("execution(* com.dragontalker.aopdemo.dao.*.get*(..))")
-	private void getter() {}
-	
-	// create point cut for setter methods
-	@Pointcut("execution(* com.dragontalker.aopdemo.dao.*.set*(..))")
-	private void setter() {}
-	
-	// combine point cut: include package ... exclude getter/setter
-	@Pointcut("forDaoPackage() && !(getter() || setter())")
-	private void forDaoPackageNoGetterSetter() {}
 	
 	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddMethod() {
@@ -30,11 +15,7 @@ public class MyDemoLoggingAspect {
 		System.out.println("\n=====>> Excuting @Before advice on any add method!");
 	}
 	
-	@Before("forDaoPackageNoGetterSetter()")
-	public void performApiAnalytics() {
-		
-		System.out.println("\n=====>> Performing API analytics");
-	}
+	
 	
 	@Before("forDaoPackageNoGetterSetter()")
 	public void logToCloudAsync() {
