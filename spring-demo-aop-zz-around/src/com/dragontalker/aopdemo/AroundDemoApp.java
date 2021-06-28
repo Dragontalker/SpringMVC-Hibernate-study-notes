b80bc7456d55a1e261a9d4c3a31baa01c53cf414
Richard <richard.yang.tong@gmail.com>
2021-06-28 16:21:22 -0400

    Added notes.

---
+++ spring-demo-aop-zz-around/src/com/dragontalker/aopdemo/AroundDemoApp.java
@@ -0,0 +1,43 @@
+package com.dragontalker.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.dragontalker.aopdemo.dao.AccountDAO;
+
+public class AroundDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring configuration java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		// call method to find the accounts
+		List<Account> theAccounts = null;
+		
+		try {
+			// add a boolean flag to simulate exception
+			boolean tripWire = true;
+			
+			theAccounts = theAccountDAO.findAccounts(tripWire);
+		} catch (Exception e) {
+			System.out.println("\n>Error> Main Program ... caught exception: " + e);
+		}
+		
+		// display the accounts
+		System.out.println("\n\n>> Main Program: AfterThrowingDemoApp");
+		System.out.println(">> -----");
+		
+		System.out.println(">> " + theAccounts);
+		
+		System.out.println(">> -----\n");
+		
+		// close the context
+		context.close();
+	}
+}
