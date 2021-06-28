8d8f4689c546791d916dacb262913caabfdb8565
Richard <richard.yang.tong@gmail.com>
2021-06-28 09:38:16 -0400

    Added notes.

--- spring-demo-aop/src/com/dragontalker/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/dragontalker/aopdemo/MainDemoApp.java
@@ -4,5 +4,13 @@ public class MainDemoApp {
 
 	public static void main(String[] args) {
 		
+		// read spring config java class
+		
+		// get the bean from spring container
+		
+		// call the bussiness method
+		
+		// close the context
+		
 	}
 }
