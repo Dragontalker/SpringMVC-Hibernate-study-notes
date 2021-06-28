fcb5d71324817a6d45f667a7fec7920813a4e818
Richard <richard.yang.tong@gmail.com>
2021-06-28 16:20:05 -0400

    Added notes.

--- spring-demo-aop-zz-around/src/com/dragontalker/aopdemo/service/TrafficFortuneService.java
+++ spring-demo-aop-zz-around/src/com/dragontalker/aopdemo/service/TrafficFortuneService.java
@@ -1,12 +1,21 @@
 package com.dragontalker.aopdemo.service;
 
+import java.util.concurrent.TimeUnit;
+
 public class TrafficFortuneService {
 
 	public String getFortune() {
 		
 		// simulate a delay
+		try {
+			
+			TimeUnit.SECONDS.sleep(5);
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		
 		// return a fortune
-		
+		return "Expect heavy traffic this morning";
 	}
 }
