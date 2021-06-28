be26e6284267896f3bc8ddda5c5b31c073207f40
Richard <richard.yang.tong@gmail.com>
2021-06-28 12:07:24 -0400

    Added notes.

--- spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/LuvAopExpressions.java
+++ spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/LuvAopExpressions.java
@@ -1,10 +1,25 @@
 package com.dragontalker.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LuvAopExpressions {
 
+	@Pointcut("execution(* com.dragontalker.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	// create point cut for getter methods
+	@Pointcut("execution(* com.dragontalker.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	// create point cut for setter methods
+	@Pointcut("execution(* com.dragontalker.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	// combine point cut: include package ... exclude getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
 }
