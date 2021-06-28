28873cd36e74fb579519c7da9e7f8182dbdaecf2
Richard <richard.yang.tong@gmail.com>
2021-06-28 16:41:53 -0400

    Added notes.

--- spring-demo-aop-zz-around-with-logger/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-zz-around-with-logger/src/com/dragontalker/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -30,7 +30,7 @@ public class MyDemoLoggingAspect {
 		
 		// print out method we are advising on
 		String method = theProceedingJoinPoint.getSignature().toShortString();
-		System.out.println("\n>> Executing @Around on method: " + method);
+		myLogger.info("\n>> Executing @Around on method: " + method);
 		
 		// get begin time stamp
 		long begin = System.currentTimeMillis();
@@ -43,7 +43,7 @@ public class MyDemoLoggingAspect {
 		
 		// compute duration and display it
 		long duration = end - begin;
-		System.out.println("\n>> Duration: " + duration / 1000.0 + " seconds");
+		myLogger.info("\n>> Duration: " + duration / 1000.0 + " seconds");
 		
 		return result;
 	}
@@ -53,7 +53,7 @@ public class MyDemoLoggingAspect {
 		
 		// print out which method we are advising on
 		String method = theJoinPoint.getSignature().toShortString();
-		System.out.println("\n>>> Executing @After (finally) on method: " + method);
+		myLogger.info("\n>>> Executing @After (finally) on method: " + method);
 	}
 	
 	@AfterThrowing(
@@ -65,10 +65,10 @@ public class MyDemoLoggingAspect {
 		
 		// print out which method we are advising on
 		String method = theJoinPoint.getSignature().toShortString();
-		System.out.println("\n>>> Executing @AfterThrowing on method: " + method);
+		myLogger.info("\n>>> Executing @AfterThrowing on method: " + method);
 		
 		// log the exception
-		System.out.println("\n>>> exception is: " + theExc);
+		myLogger.info("\n>>> exception is: " + theExc);
 		
 	}
 	
@@ -82,17 +82,17 @@ public class MyDemoLoggingAspect {
 		
 		// print out which method we are advising on
 		String method = theJoinPoint.getSignature().toShortString();
-		System.out.println("\n>>> Executing @AfterReturning on method: " + method);
+		myLogger.info("\n>>> Executing @AfterReturning on method: " + method);
 		
 		// print out the results of the method call
-		System.out.println("\n>>> result is: " + result);
+		myLogger.info("\n>>> result is: " + result);
 		
 		// let's post-process the data ... let's modify it!
 		
 		// convert the account names to upper case
 		convertAccountNamesToUpperCase(result);
 		
-		System.out.println("\n>>> modified result is: " + result);
+		myLogger.info("\n>>> modified result is: " + result);
 	}
 	
 	private void convertAccountNamesToUpperCase(List<Account> result) {
@@ -113,12 +113,12 @@ public class MyDemoLoggingAspect {
 	@Before("com.dragontalker.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddMethod(JoinPoint theJoinPoint) {
 		
-		System.out.println("\n=====>> Excuting @Before advice on any add method!");
+		myLogger.info("\n=====>> Excuting @Before advice on any add method!");
 		
 		// display the method signature
 		MethodSignature methodSignature = (MethodSignature) theJoinPoint.getSignature();
 		
-		System.out.println(">> Method: " + methodSignature);
+		myLogger.info(">> Method: " + methodSignature);
 		
 		// display method arguments
 		
@@ -127,15 +127,15 @@ public class MyDemoLoggingAspect {
 		
 		// loop thru args
 		for (int i = 0; i < argObjects.length; i++) {
-			System.out.println(">> #" + i + " argument: " + argObjects[i]);
+			myLogger.info(">> #" + i + " argument: " + argObjects[i]);
 			
 			if (argObjects[i] instanceof Account) {
 				
 				// downcast and print Account specific stuff
 				Account theAccount = (Account) argObjects[i];
 				
-				System.out.println(">>> account name: " + theAccount.getName());
-				System.out.println(">>> account level: " + theAccount.getLevel());
+				myLogger.info(">>> account name: " + theAccount.getName());
+				myLogger.info(">>> account level: " + theAccount.getLevel());
 			}
 		}
 		
