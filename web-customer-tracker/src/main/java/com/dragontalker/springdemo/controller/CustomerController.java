5ef510edd7a348fd7be41cd20c26ef78bd12d541
Richard <richard.yang.tong@gmail.com>
2021-06-27 20:03:33 -0400

    Added notes.

--- web-customer-tracker/src/main/java/com/dragontalker/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/dragontalker/springdemo/controller/CustomerController.java
@@ -47,6 +47,9 @@ public class CustomerController {
 	@PostMapping("/saveCustomer")
 	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
 		
+		// save the customer using our service
+		customerService.saveCustomer(theCustomer);
+		
 		return "redirect:/customer/list";
 	}
 	
