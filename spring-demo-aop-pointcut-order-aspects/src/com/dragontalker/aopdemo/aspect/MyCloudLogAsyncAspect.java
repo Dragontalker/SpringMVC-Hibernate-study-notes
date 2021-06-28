79c9e35da146a9f22f85486594a45487493f5ad2
Richard <richard.yang.tong@gmail.com>
2021-06-28 12:14:39 -0400

    Added notes.

--- spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ spring-demo-aop-pointcut-order-aspects/src/com/dragontalker/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,10 +2,12 @@ package com.dragontalker.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class MyCloudLogAsyncAspect {
 
 	@Before("com.dragontalker.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
