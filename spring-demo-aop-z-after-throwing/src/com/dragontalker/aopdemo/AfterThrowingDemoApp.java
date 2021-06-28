75004b6941f68dfad8e91f0fb53ce10838991ff8
Richard <richard.yang.tong@gmail.com>
2021-06-28 14:31:25 -0400

    Added notes.

---
+++ spring-demo-aop-z-after-throwing/src/com/dragontalker/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,34 @@
+package com.dragontalker.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.dragontalker.aopdemo.dao.AccountDAO;
+
+public class AfterThrowingDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring configuration java class
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		// call method to find the accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts();
+		
+		// display the accounts
+		System.out.println("\n\n>> Main Program: AfterThrowingDemoApp");
+		System.out.println(">> -----");
+		
+		System.out.println(">> " + theAccounts);
+		
+		System.out.println(">> -----\n");
+		
+		// close the context
+		context.close();
+	}
+}
