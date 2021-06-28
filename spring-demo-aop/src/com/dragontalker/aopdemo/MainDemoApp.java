400e62634de1344798a7eaafb2cab92627ddb3e2
Richard <richard.yang.tong@gmail.com>
2021-06-28 09:40:19 -0400

    Added notes.

--- spring-demo-aop/src/com/dragontalker/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/dragontalker/aopdemo/MainDemoApp.java
@@ -2,6 +2,8 @@ package com.dragontalker.aopdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.dragontalker.aopdemo.dao.AccountDAO;
+
 public class MainDemoApp {
 
 	public static void main(String[] args) {
@@ -11,10 +13,12 @@ public class MainDemoApp {
 				new AnnotationConfigApplicationContext(DemoConfig.class);
 		
 		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 		
 		// call the business method
+		theAccountDAO.addAccount();
 		
 		// close the context
-		
+		context.close();
 	}
 }
