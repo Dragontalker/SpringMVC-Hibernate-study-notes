d4026bf43ae3d126afab36ebbee7ad11cb3f48ff
Richard <richard.yang.tong@gmail.com>
2021-06-29 14:18:15 -0400

    Added notes.

--- spring-security-demo-02-basic-security/src/main/java/com/dragontalker/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-02-basic-security/src/main/java/com/dragontalker/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,7 +1,11 @@
 package com.dragontalker.springsecurity.demo.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
 }
