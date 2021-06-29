b2cafd2b02f8d23eddccbacebc473417049761d5
Richard <richard.yang.tong@gmail.com>
2021-06-29 15:14:06 -0400

    Added notes.

--- spring-security-demo-03-custom-login-form/src/main/java/com/dragontalker/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/dragontalker/springsecurity/demo/controller/LoginController.java
@@ -1,5 +1,14 @@
 package com.dragontalker.springsecurity.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
 
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		
+		return "plain-login";
+	}
 }
