980ea82cf7962b7ad8b3e3bbf1b9d08e5f826f1c
Richard <richard.yang.tong@gmail.com>
2021-06-29 17:44:48 -0400

    Added notes.

--- spring-security-demo-07-user-roles/src/main/java/com/dragontalker/springsecurity/demo/controller/DemoController.java
+++ spring-security-demo-07-user-roles/src/main/java/com/dragontalker/springsecurity/demo/controller/DemoController.java
@@ -11,4 +11,11 @@ public class DemoController {
 		
 		return "home";
 	}
+	
+	// add request mapping for /leaders
+	@GetMapping("/leaders")
+	public String showLeaders() {
+		
+		return "leaders";
+	}
 }
