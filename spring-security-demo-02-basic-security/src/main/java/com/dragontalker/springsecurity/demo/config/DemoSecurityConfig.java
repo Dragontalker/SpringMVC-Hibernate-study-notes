0268fe55e8772b68891aa8b4923243ecce04e5bd
Richard <richard.yang.tong@gmail.com>
2021-06-29 14:18:48 -0400

    Added notes.

--- spring-security-demo-02-basic-security/src/main/java/com/dragontalker/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-02-basic-security/src/main/java/com/dragontalker/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.dragontalker.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(auth);
+	}
+
+	
 }
