4767fec83ae4203582f13eb5a0308f810775db1f
Richard <richard.yang.tong@gmail.com>
2021-06-29 13:33:58 -0400

    Added notes.

--- Maven/spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/com/dragontalker/springsecurity/demo/config/DemoAppConfig.java
+++ Maven/spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/com/dragontalker/springsecurity/demo/config/DemoAppConfig.java
@@ -1,5 +1,12 @@
 package com.dragontalker.springsecurity.demo.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.dragontalker.springsecurity.demo")
 public class DemoAppConfig {
 
 }
