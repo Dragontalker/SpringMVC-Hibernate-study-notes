1ad81c8d354b190bf929794aaa696baf473d728b
Richard <richard.yang.tong@gmail.com>
2021-06-29 15:10:30 -0400

    Added notes.

--- spring-security-demo-03-custom-login-form/src/main/java/com/dragontalker/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/dragontalker/springsecurity/demo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.dragontalker.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -21,6 +22,12 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
 			.withUser(users.username("mary").password("test123").roles("MANAGER"))
 			.withUser(users.username("susan").password("test123").roles("ADMIN"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(http);
 	}
 
 	
