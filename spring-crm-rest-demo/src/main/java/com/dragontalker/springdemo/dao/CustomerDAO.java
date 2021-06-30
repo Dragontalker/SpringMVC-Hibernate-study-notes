c3c495db8c4c8b29d8e5c34f679026eaf7e41259
Richard <richard.yang.tong@gmail.com>
2021-06-30 14:37:58 -0400

    Added notes.

---
+++ spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.dragontalker.springdemo.dao;
+
+import java.util.List;
+
+import com.dragontalker.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
