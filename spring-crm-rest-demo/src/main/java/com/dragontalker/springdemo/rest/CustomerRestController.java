0cd5f09942f8604fd0bd823718d5e5ffb76d6f05
Richard <richard.yang.tong@gmail.com>
2021-06-30 16:15:05 -0400

    Added notes.

--- spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +37,13 @@ public class CustomerRestController {
 		if (theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id is NOT FOUND - " + customerId);
 		}
+		
+		return theCustomer;
+	}
+	
+	// add mapping for POST /customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
 		
 		return theCustomer;
 	}
