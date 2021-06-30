c8794f1ad3ff1de224cac2a67c419b7e365cb286
Richard <richard.yang.tong@gmail.com>
2021-06-30 15:06:56 -0400

    Added notes.

--- spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerRestController.java
@@ -1,14 +1,18 @@
 package com.dragontalker.springdemo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dragontalker.springdemo.service.CustomerService;
 
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
 
 	// autowire the CustomerService
-	
+	@Autowired
+	private CustomerService customerService;
 	
 	// add mapping for GET /customers
 	
