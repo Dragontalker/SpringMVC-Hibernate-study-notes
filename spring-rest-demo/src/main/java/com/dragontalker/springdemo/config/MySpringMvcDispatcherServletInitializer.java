15e77c1f56dc64e708f76960bdd096d83f3b922e
Richard <richard.yang.tong@gmail.com>
2021-06-30 11:01:42 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/config/MySpringMvcDispatcherServletInitializer.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/config/MySpringMvcDispatcherServletInitializer.java
@@ -18,8 +18,8 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
 
 	@Override
 	protected String[] getServletMappings() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return new String[] { "/" };
 	}
 
 }
