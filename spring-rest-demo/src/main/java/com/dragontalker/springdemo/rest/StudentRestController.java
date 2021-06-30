9e0341bd967067a40d3820c1a2d28bf724e1c65e
Richard <richard.yang.tong@gmail.com>
2021-06-30 11:29:12 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestController.java
@@ -1,10 +1,21 @@
 package com.dragontalker.springdemo.rest;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dragontalker.springdemo.entity.Student;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
+	// define end point for "/student" - return list of students
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+		
+		return null;
+	}
 }
