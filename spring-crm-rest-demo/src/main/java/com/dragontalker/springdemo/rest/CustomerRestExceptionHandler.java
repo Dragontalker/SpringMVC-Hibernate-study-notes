3b69662a68395743ca535328eb1433c118e4afdc
Richard <richard.yang.tong@gmail.com>
2021-06-30 16:00:52 -0400

    Added notes.

--- spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerRestExceptionHandler.java
+++ spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerRestExceptionHandler.java
@@ -1,11 +1,19 @@
 package com.dragontalker.springdemo.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomerRestExceptionHandler {
 	
 	// Add an exception handler for CustomerNotFoundException
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(
+			CustomerNotFoundException exception) {
+		
+		return null;
+	}
 	
 	// Add another exception handler ... to catch any exception (catch all)
 
