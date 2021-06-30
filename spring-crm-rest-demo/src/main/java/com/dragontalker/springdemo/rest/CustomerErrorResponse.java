072b6e998338b47eeb152546c8b09a038706ff32
Richard <richard.yang.tong@gmail.com>
2021-06-30 15:54:40 -0400

    Added notes.

--- spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerErrorResponse.java
+++ spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerErrorResponse.java
@@ -13,5 +13,29 @@ public class CustomerErrorResponse {
 		this.message = message;
 		this.timeStamp = timeStamp;
 	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
 	
 }
