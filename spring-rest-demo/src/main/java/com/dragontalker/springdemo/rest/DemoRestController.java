8fd57a917fccc843ccc87af8dc93815797abd400
Richard <richard.yang.tong@gmail.com>
2021-06-30 11:04:03 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/DemoRestController.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/DemoRestController.java
@@ -1,5 +1,6 @@
 package com.dragontalker.springdemo.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class DemoRestController {
 
+	// add code for the "/hello" end point
+	@GetMapping("/hello")
+	public String sayHello() {
+		
+		return "Hello World!";
+	}
 }
