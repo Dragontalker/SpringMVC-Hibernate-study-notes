a872c06e43e83907713c9b12097220cafc0ab150
Richard <richard.yang.tong@gmail.com>
2021-06-30 09:52:05 -0400

    Added notes.

--- jackson-databinding-json-demo/src/main/java/com/dragontalker/jackson/json/demo/Driver.java
+++ jackson-databinding-json-demo/src/main/java/com/dragontalker/jackson/json/demo/Driver.java
@@ -1,5 +1,15 @@
 package com.dragontalker.jackson.json.demo;
 
 public class Driver {
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
