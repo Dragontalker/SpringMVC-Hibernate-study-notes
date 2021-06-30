b4c8503f9a7dfb33cd8839a447c9d0f6a8f78ff9
Richard <richard.yang.tong@gmail.com>
2021-06-30 11:03:15 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/DemoRestController.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/DemoRestController.java
@@ -1,5 +1,10 @@
 package com.dragontalker.springdemo.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class DemoRestController {
 
 }
