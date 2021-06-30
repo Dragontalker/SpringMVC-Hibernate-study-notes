c864d4740af46e1a924eb0b381125056a347868c
Richard <richard.yang.tong@gmail.com>
2021-06-30 10:00:22 -0400

    Added notes.

--- jackson-databinding-json-demo/src/main/java/com/dragontalker/jackson/json/demo/Address.java
+++ jackson-databinding-json-demo/src/main/java/com/dragontalker/jackson/json/demo/Address.java
@@ -1,5 +1,10 @@
 package com.dragontalker.jackson.json.demo;
 
 public class Address {
-
+	
+	private String street;
+	private String city;
+	private String state;
+	private String zip;
+	private String country;
 }
