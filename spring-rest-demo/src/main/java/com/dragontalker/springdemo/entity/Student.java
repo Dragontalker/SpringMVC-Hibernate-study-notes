fcd1bf32b17f1cfc6696727f06fc6d693d2f5e17
Richard <richard.yang.tong@gmail.com>
2021-06-30 11:22:50 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/entity/Student.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/entity/Student.java
@@ -11,5 +11,21 @@ public class Student {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 	
 }
