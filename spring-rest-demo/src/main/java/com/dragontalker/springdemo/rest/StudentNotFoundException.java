a2fb02ac033a484c1fd8599cb7c91fad412290bf
Richard <richard.yang.tong@gmail.com>
2021-06-30 12:06:40 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentNotFoundException.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentNotFoundException.java
@@ -1,5 +1,17 @@
 package com.dragontalker.springdemo.rest;
 
-public class StudentNotFoundException {
+public class StudentNotFoundException extends RuntimeException{
+
+	public StudentNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public StudentNotFoundException(String message) {
+		super(message);
+	}
+
+	public StudentNotFoundException(Throwable cause) {
+		super(cause);
+	}
 
 }
