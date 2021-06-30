19fd9f1b9ee6efcff318fe008c3080d24934a43c
Richard <richard.yang.tong@gmail.com>
2021-06-30 16:05:46 -0400

    Added notes.

--- spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerRestExceptionHandler.java
+++ spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerRestExceptionHandler.java
@@ -22,8 +22,24 @@ public class CustomerRestExceptionHandler {
 		
 		// return ResponseEntity
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		
 	}
 	
 	// Add another exception handler ... to catch any exception (catch all)
-
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(
+			Exception exception) {
+		
+		// create CustomerErrorResponse
+		CustomerErrorResponse error = 
+				new CustomerErrorResponse(
+						HttpStatus.BAD_REQUEST.value(),
+						exception.getMessage(),
+						System.currentTimeMillis());
+		
+		// return ResponseEntity
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		
+	}
+	
 }
