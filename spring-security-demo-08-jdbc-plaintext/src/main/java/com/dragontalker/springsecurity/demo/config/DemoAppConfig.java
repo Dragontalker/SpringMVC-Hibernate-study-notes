41cb0539ad02d4cf1a1af2f0217b52367373c6c8
Richard <richard.yang.tong@gmail.com>
2021-06-29 21:04:09 -0400

    Added notes.

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/dragontalker/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/dragontalker/springsecurity/demo/config/DemoAppConfig.java
@@ -1,9 +1,11 @@
 package com.dragontalker.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -13,9 +15,12 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan(basePackages="com.dragontalker.springsecurity.demo")
 @PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+	
+	// set up variable to hold the properties
+	@Autowired
+	private Environment env;
 
 	// define a bean for ViewResolver
-
 	@Bean
 	public ViewResolver viewResolver() {
 		
