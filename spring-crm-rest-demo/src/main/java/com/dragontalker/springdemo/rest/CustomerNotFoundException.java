daaef245e22bd9078ffb579ab42ae7eb7bd7fad3
Richard <richard.yang.tong@gmail.com>
2021-06-30 15:57:19 -0400

    Added notes.

--- spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerNotFoundException.java
+++ spring-crm-rest-demo/src/main/java/com/dragontalker/springdemo/rest/CustomerNotFoundException.java
@@ -1,5 +1,24 @@
 package com.dragontalker.springdemo.rest;
 
-public class CustomerNotFoundException extends RuntimeException{
+public class CustomerNotFoundException extends RuntimeException {
+
+	public CustomerNotFoundException() {}
+
+	public CustomerNotFoundException(String message) {
+		super(message);
+	}
+
+	public CustomerNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
 
 }
