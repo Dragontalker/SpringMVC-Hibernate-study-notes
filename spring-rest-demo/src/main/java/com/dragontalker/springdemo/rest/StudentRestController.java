ecc94fef2f3fa6ec420a9c3089d5642f0016e7b2
Richard <richard.yang.tong@gmail.com>
2021-06-30 14:08:55 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestController.java
@@ -52,37 +52,4 @@ public class StudentRestController {
 		
 	}
 	
-	// Add an exception handler using @ExceptionHandler
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(
-			StudentNotFoundException exception) {
-		
-		// create a StudentErrorResponse
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exception.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		// return ResponseEntity
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-		
-	}
-	
-	// add another exception handler ... to catch any exception (catch all)
-	@ExceptionHandler
-	public ResponseEntity<StudentErrorResponse> handleException(Exception exception)  {
-		
-		// create a StudentErrorResponse
-		StudentErrorResponse error = new StudentErrorResponse();
-		
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage("Ops! Your request is not valid!");
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		// return ResponseEntity
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-		
-	}
-	
 }
--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestExceptionHandler.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestExceptionHandler.java
@@ -1,11 +1,45 @@
 package com.dragontalker.springdemo.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class StudentRestExceptionHandler {
 	
 	// add exception handling code here
+	// Add an exception handler using @ExceptionHandler
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(
+			StudentNotFoundException exception) {
+		
+		// create a StudentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exception.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// return ResponseEntity
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		
+	}
 	
+	// add another exception handler ... to catch any exception (catch all)
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exception)  {
+		
+		// create a StudentErrorResponse
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage("Ops! Your request is not valid!");
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// return ResponseEntity
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+		
+	}
 
 }
