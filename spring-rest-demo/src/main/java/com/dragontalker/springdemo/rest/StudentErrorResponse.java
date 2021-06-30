ef687fc10cedc014d1088ba17c6024c279e4d138
Richard <richard.yang.tong@gmail.com>
2021-06-30 12:04:00 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentErrorResponse.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentErrorResponse.java
@@ -1,7 +1,5 @@
 package com.dragontalker.springdemo.rest;
 
-import javax.print.event.PrintJobAttributeEvent;
-
 public class StudentErrorResponse {
 	
 	private int status;
@@ -13,6 +11,30 @@ public class StudentErrorResponse {
 	public StudentErrorResponse(int status, String message, long timeStamp) {
 		this.status = status;
 		this.message = message;
+		this.timeStamp = timeStamp;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(long timeStamp) {
 		this.timeStamp = timeStamp;
 	}
 
