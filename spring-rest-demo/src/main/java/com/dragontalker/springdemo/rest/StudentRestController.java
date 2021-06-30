c28c89602c184cf34ae998fffedd29640fa1675b
Richard <richard.yang.tong@gmail.com>
2021-06-30 11:46:07 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +34,12 @@ public class StudentRestController {
 	public List<Student> getStudents() {
 
 		return theStudents;
+	}
+	
+	// define end point for "/students/{studentId}" - return student at index
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		return null;
 	}
 }
