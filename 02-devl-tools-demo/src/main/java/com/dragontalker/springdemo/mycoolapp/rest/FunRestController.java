7ce9201ae86f4114e4e8aff774279126056146cc
Richard <richard.yang.tong@gmail.com>
2021-06-30 19:13:33 -0400

    Added notes.

---
+++ 02-devl-tools-demo/src/main/java/com/dragontalker/springdemo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,18 @@
+package com.dragontalker.springdemo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	// expose "/" that return "Hello World"
+	@GetMapping("/")
+	public String sayHello() {
+		
+		return "Hello World! Time on server is " + LocalDateTime.now();
+	}
+	
+}
