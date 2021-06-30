379828fde24f74e35f14d5cf95ecf9c9e45ce6fc
Richard <richard.yang.tong@gmail.com>
2021-06-30 14:08:27 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestExceptionHandler.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestExceptionHandler.java
@@ -1,5 +1,11 @@
 package com.dragontalker.springdemo.rest;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
 public class StudentRestExceptionHandler {
+	
+	// add exception handling code here
+	
 
 }
