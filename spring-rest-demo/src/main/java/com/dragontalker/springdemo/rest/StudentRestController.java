aeb139d6c6bd93b1a7c57c4ebef145d476c74d82
Richard <richard.yang.tong@gmail.com>
2021-06-30 11:42:32 -0400

    Added notes.

--- spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/dragontalker/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.dragontalker.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,14 @@ import com.dragontalker.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	private List<Student> theStudents;
+	
+	// define @PostConstruct to load the student data ... only once!
+	@PostConstruct
+	public void loadData() {
+		
+	}
 
 	// define end point for "/student" - return list of students
 	@GetMapping("/students")
--- spring-rest-demo/src/main/webapp/index.jsp
+++ spring-rest-demo/src/main/webapp/index.jsp
@@ -4,9 +4,18 @@
 	
 		<h3>Spring REST Demo</h3>
 		
+		<br>
+		
 		<hr>
+		<br>
 		
 		<a href="${ pageContext.request.contextPath }/test/hello">Hello</a>
+		
+		<br>
+		<hr>
+		<br>
+		
+		<a href="${ pageContext.request.contextPath }/api/students">Students</a>
 	
 	</body>
 
