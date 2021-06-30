efee91f1de2345c4e977a3abf38f9dc86ede3b00
Richard <richard.yang.tong@gmail.com>
2021-06-29 21:19:54 -0400

    Added notes.

--- spring-security-demo-08-jdbc-plaintext/src/main/java/com/dragontalker/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-08-jdbc-plaintext/src/main/java/com/dragontalker/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.dragontalker.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +14,10 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	// add a reference to our security data source
+	@Autowired
+	private DataSource securityDataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
