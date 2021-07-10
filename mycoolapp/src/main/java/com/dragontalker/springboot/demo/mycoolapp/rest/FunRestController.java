0c8f4d1cde55008f107e44eee686bbf59176f06c
Richard <richard.yang.tong@gmail.com>
2021-07-10 16:57:44 -0400

    Added notes.

--- mycoolapp/src/main/java/com/dragontalker/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/dragontalker/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,10 @@
 package com.dragontalker.springboot.demo.mycoolapp.rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
 
+	// expose "/" that return "HelloWorld"
+	
 }
