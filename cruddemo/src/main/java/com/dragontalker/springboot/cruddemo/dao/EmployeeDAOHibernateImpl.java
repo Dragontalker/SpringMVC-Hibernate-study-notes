bec9a3c739637a9be42d907fbeaad35b5f760729
Richard <richard.yang.tong@gmail.com>
2021-07-11 10:40:23 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -2,6 +2,9 @@ package com.dragontalker.springboot.cruddemo.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.dragontalker.springboot.cruddemo.entity.Employee;
@@ -10,8 +13,13 @@ import com.dragontalker.springboot.cruddemo.entity.Employee;
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 
 	// define field for entitymanager
+	private EntityManager entityManager;
 	
 	// set up constructor injection
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
 	
 	@Override
 	public List<Employee> findAll() {
