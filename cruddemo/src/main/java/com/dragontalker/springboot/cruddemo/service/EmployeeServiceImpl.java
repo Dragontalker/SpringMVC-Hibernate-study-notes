83c22a1f322b2c8affe112eb15de9ad286356ef8
Richard <richard.yang.tong@gmail.com>
2021-07-11 11:20:40 -0400

    Added notes.

---
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,33 @@
+package com.dragontalker.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.dragontalker.springboot.cruddemo.entity.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
