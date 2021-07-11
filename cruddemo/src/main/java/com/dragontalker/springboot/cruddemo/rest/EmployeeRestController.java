4fdd49771d31742ff209cc41df2ed522b0c3d003
Richard <richard.yang.tong@gmail.com>
2021-07-11 11:47:46 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,11 @@ package com.dragontalker.springboot.cruddemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,6 +59,32 @@ public class EmployeeRestController {
 		employeeService.save(theEmployee);
 		
 		return theEmployee;
+	}
+	
+	// add mapping for PUT /employees - update existing employee
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+	}
+	
+	// add mapping for DELETE /employees/{employeeId} - delete employee
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee tempEmployee = employeeService.findById(employeeId);
+		
+		// throw exception if null
+		
+		if (tempEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		
+		employeeService.deleteById(employeeId);
+		
+		return "Deleted employee id - " + employeeId;
 	}
 	
 }
