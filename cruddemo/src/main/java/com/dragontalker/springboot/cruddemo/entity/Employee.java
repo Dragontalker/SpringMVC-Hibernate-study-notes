4f1de72ccce947708b7cfb2ec827c5cc1e0be130
Richard <richard.yang.tong@gmail.com>
2021-07-11 10:31:14 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/entity/Employee.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/entity/Employee.java
@@ -1,6 +1,10 @@
 package com.dragontalker.springboot.cruddemo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -9,6 +13,9 @@ public class Employee {
 
 	// define fields
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
 	
 	private String firstName;
