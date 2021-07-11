486444855eb6db63460624103301677afe9bc0a9
Richard <richard.yang.tong@gmail.com>
2021-07-11 10:34:32 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/entity/Employee.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/entity/Employee.java
@@ -39,9 +39,45 @@ public class Employee {
 		this.email = email;
 	}
 	
-	
 	// define getter/setter
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	// define toString
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
 	
 }
