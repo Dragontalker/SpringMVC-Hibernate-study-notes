2052d0f4e8a06b8b30406ab3d2dc5676877f8802
Richard <richard.yang.tong@gmail.com>
2021-07-11 17:34:04 -0400

    Added notes.

---
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/dragontalker/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.dragontalker.springboot.thymeleafdemo.service;
+
+import java.util.List;
+
+import com.dragontalker.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
