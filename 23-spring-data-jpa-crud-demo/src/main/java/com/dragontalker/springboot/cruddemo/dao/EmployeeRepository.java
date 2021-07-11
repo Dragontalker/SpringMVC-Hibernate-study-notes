9417423c44acde5f6526b0843ed4846055aeea63
Richard <richard.yang.tong@gmail.com>
2021-07-11 15:11:10 -0400

    Added notes.

--- 23-spring-data-jpa-crud-demo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeRepository.java
+++ 23-spring-data-jpa-crud-demo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.dragontalker.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<T, ID> {
+import com.dragontalker.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
