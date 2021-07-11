3e322dde0d95c3f37be5a1512a8baa48db5526d7
Richard <richard.yang.tong@gmail.com>
2021-07-11 10:37:20 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeDAO.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeDAO.java
@@ -1,5 +1,10 @@
 package com.dragontalker.springboot.cruddemo.dao;
 
+import java.util.List;
+
+import com.dragontalker.springboot.cruddemo.entity.Employee;
+
 public interface EmployeeDAO {
 
+	public List<Employee> findAll();
 }
