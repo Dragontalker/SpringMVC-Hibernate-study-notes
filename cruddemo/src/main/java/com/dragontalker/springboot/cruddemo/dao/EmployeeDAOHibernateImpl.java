945b356728fc03e293146837e78b8d6345726809
Richard <richard.yang.tong@gmail.com>
2021-07-11 10:37:59 -0400

    Added notes.

---
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,15 @@
+package com.dragontalker.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import com.dragontalker.springboot.cruddemo.entity.Employee;
+
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
