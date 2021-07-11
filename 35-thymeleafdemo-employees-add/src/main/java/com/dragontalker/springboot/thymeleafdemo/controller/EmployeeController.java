59096a079040bb3b0197f58ff577b754f0042939
Richard <richard.yang.tong@gmail.com>
2021-07-11 17:56:34 -0400

    Added notes.

--- 35-thymeleafdemo-employees-add/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 35-thymeleafdemo-employees-add/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dragontalker.springboot.thymeleafdemo.entity.Employee;
@@ -32,5 +33,16 @@ public class EmployeeController {
 		theModel.addAttribute("employees", theEmployees);
 		
 		return "employees/list-employees";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
 	}
 }
