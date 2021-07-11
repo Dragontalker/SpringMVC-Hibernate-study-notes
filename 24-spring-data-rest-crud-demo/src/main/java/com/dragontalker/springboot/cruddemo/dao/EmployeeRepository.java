7d8637f9270c93097557939c9a8a1b7563753d5e
Richard <richard.yang.tong@gmail.com>
2021-07-11 16:14:22 -0400

    Added notes.

--- 24-spring-data-rest-crud-demo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeRepository.java
+++ 24-spring-data-rest-crud-demo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.dragontalker.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.dragontalker.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 	// that's it ... no need to write any code LOL!
