751299853b362aa4343083ca5035c9dafc558bbd
Richard <richard.yang.tong@gmail.com>
2021-07-11 17:06:22 -0400

    Added notes.

--- 32-thymeleafdemo-employees-list/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 32-thymeleafdemo-employees-list/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dragontalker.springboot.thymeleafdemo.model.Employee;
@@ -36,5 +38,12 @@ public class EmployeeController {
 	}
 	
 	// add mapping for "/list"
-	
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
 }
