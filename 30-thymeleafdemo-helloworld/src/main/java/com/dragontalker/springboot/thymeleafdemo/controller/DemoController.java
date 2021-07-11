34fabe7acaa8433f18f0f0097b2392b779864a55
Richard <richard.yang.tong@gmail.com>
2021-07-11 16:37:51 -0400

    Added notes.

---
+++ 30-thymeleafdemo-helloworld/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,10 @@
+package com.dragontalker.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoController {
+
+	// create a mapping for "/hello"
+	
+}
