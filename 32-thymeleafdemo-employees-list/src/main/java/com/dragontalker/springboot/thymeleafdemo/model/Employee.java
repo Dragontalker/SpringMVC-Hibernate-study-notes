b4143c8833d6e028b89afe3f3f00667156b1d441
Richard <richard.yang.tong@gmail.com>
2021-07-11 16:57:12 -0400

    Added notes.

--- 32-thymeleafdemo-employees-list/src/main/java/com/dragontalker/springboot/thymeleafdemo/model/Employee.java
+++ 32-thymeleafdemo-employees-list/src/main/java/com/dragontalker/springboot/thymeleafdemo/model/Employee.java
@@ -2,4 +2,8 @@ package com.dragontalker.springboot.thymeleafdemo.model;
 
 public class Employee {
 
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
 }
