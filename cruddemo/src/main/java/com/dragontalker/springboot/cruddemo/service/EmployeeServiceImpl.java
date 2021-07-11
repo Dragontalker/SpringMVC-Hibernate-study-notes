f33b8432a070ade18d8b17bae4dba472438138c9
Richard <richard.yang.tong@gmail.com>
2021-07-11 11:22:38 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,12 +2,21 @@ package com.dragontalker.springboot.cruddemo.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dragontalker.springboot.cruddemo.dao.EmployeeDAO;
 import com.dragontalker.springboot.cruddemo.entity.Employee;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+	
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
 
 	@Override
 	public List<Employee> findAll() {
