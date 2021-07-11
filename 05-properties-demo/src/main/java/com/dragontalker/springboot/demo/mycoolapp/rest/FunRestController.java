ead63cf016dc6071e24f9dd27f2a43a31785352b
Richard <richard.yang.tong@gmail.com>
2021-07-11 09:33:04 -0400

    Added notes.

--- 05-properties-demo/src/main/java/com/dragontalker/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 05-properties-demo/src/main/java/com/dragontalker/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,11 +2,19 @@ package com.dragontalker.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	// inject properties for: coach.name and team.name
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
 
 	// expose "/" that return "HelloWorld"
 	@GetMapping("/")
