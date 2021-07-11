e5c30a2e76b2dc2f93aa5f3c8df6ffee9eab2f33
Richard <richard.yang.tong@gmail.com>
2021-07-11 10:59:05 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,13 +1,22 @@
 package com.dragontalker.springboot.cruddemo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dragontalker.springboot.cruddemo.dao.EmployeeDAO;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
+	private EmployeeDAO employeeDAO;
+	
 	// quick and dirty: inject employee DAO
+	@Autowired
+	public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
 	
 	// expose "/employees"and return list of employees
 	
