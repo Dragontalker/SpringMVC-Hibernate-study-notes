0ba249a540fcf48e1ccc0048ce4f3bfeb7e320e1
Richard <richard.yang.tong@gmail.com>
2021-07-11 11:35:55 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +43,12 @@ public class EmployeeRestController {
 		}
 		
 		return theEmployee;
+	}
+	
+	// add mapping for POST /employees - add new employee
+	@PostMapping("/employees/{employeeId}")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		
 	}
 	
 }
