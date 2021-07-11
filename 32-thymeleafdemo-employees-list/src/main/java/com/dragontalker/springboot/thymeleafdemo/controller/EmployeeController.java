27131db5e1a57c2b633bc11fa401c57a082ceca1
Richard <richard.yang.tong@gmail.com>
2021-07-11 17:00:20 -0400

    Added notes.

--- 32-thymeleafdemo-employees-list/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 32-thymeleafdemo-employees-list/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,13 +1,19 @@
 package com.dragontalker.springboot.thymeleafdemo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dragontalker.springboot.thymeleafdemo.model.Employee;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
 	// load employee data
+	
+	private List<Employee> employees;
 	
 	// add mapping for "/list"
 	
