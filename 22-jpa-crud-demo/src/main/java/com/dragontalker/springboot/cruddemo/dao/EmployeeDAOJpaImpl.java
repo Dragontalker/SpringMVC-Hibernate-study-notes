050d4def4a630404d0fcc49be591023170acdb6e
Richard <richard.yang.tong@gmail.com>
2021-07-11 14:40:29 -0400

    Added notes.

--- 22-jpa-crud-demo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
+++ 22-jpa-crud-demo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -2,12 +2,22 @@ package com.dragontalker.springboot.cruddemo.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.dragontalker.springboot.cruddemo.entity.Employee;
 
 @Repository
 public class EmployeeDAOJpaImpl implements EmployeeDAO {
+	
+	private EntityManager entityManager;
+	
+	@Autowired
+	public EmployeeDAOJpaImpl(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
 
 	@Override
 	public List<Employee> findAll() {
