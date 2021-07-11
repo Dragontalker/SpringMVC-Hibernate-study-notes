e24a73de72d1298a2c2684ebcebe538b69c97d02
Richard <richard.yang.tong@gmail.com>
2021-07-11 17:32:53 -0400

    Added notes.

---
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/dragontalker/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.dragontalker.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dragontalker.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+}
