34e807366c10882057f58980dada13436eaf753f
Richard <richard.yang.tong@gmail.com>
2021-07-11 10:33:09 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/entity/Employee.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/entity/Employee.java
@@ -29,6 +29,17 @@ public class Employee {
 	
 	// define constructors
 	
+	public Employee() {
+		
+	}
+
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	
 	// define getter/setter
 	
 	// define toString
