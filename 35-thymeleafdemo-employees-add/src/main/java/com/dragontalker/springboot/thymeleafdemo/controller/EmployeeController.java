d5ca0d758c510e297fad03080fb4e67fcb29e0de
Richard <richard.yang.tong@gmail.com>
2021-07-11 18:07:25 -0400

    Added notes.

--- 35-thymeleafdemo-employees-add/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 35-thymeleafdemo-employees-add/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dragontalker.springboot.thymeleafdemo.entity.Employee;
@@ -43,5 +45,15 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 		
 		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		
+		// save the employee
+		employeeService.save(theEmployee);
+		
+		// use a redirect to prevent duplicate submission
+		return "redirect:employees/list";
 	}
 }
