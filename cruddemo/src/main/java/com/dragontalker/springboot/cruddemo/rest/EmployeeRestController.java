be037ada0ea61514b1a77cecc02afd93a8a6d78d
Richard <richard.yang.tong@gmail.com>
2021-07-11 10:57:37 -0400

    Added notes.

--- cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/dragontalker/springboot/cruddemo/rest/EmployeeRestController.java
@@ -2,4 +2,8 @@ package com.dragontalker.springboot.cruddemo.rest;
 
 public class EmployeeRestController {
 
+	// quick and dirty: inject employee DAO
+	
+	// expose "/employees"and return list of employees
+	
 }
