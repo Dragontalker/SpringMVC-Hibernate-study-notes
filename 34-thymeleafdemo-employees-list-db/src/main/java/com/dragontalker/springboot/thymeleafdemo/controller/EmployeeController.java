e18efd9260a53ccb5418813b672ae57441c189e3
Richard <richard.yang.tong@gmail.com>
2021-07-11 17:36:22 -0400

    Added notes.

--- 34-thymeleafdemo-employees-list-db/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -18,26 +18,6 @@ import com.dragontalker.springboot.thymeleafdemo.entity.Employee;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	// load employee data
-	
-	private List<Employee> theEmployees;
-	
-	@PostConstruct
-	private void loadData() {
-		
-		// create employees
-		Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee(2, "Emma", "Baugarten", "emma@luv2code.com");
-		Employee emp3 = new Employee(3, "Avani", "Gupta", "avani@luv2code.com");
-
-		// create the list
-		theEmployees = new ArrayList<>();
-		
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
-	}
 	
 	// add mapping for "/list"
 	@GetMapping("/list")
