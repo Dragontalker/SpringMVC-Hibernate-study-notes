80c44e0167f32bd373685255e007f95d2960d1a1
Richard <richard.yang.tong@gmail.com>
2021-07-11 16:59:30 -0400

    Added notes.

--- 32-thymeleafdemo-employees-list/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 32-thymeleafdemo-employees-list/src/main/java/com/dragontalker/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.dragontalker.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/employees")
 public class EmployeeController {
 
 }
