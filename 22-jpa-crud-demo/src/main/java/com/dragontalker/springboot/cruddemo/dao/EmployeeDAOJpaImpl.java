93f1d120b3c970b8c4ea8b8a08059f610bcc582e
Richard <richard.yang.tong@gmail.com>
2021-07-11 14:39:15 -0400

    Added notes.

---
+++ 22-jpa-crud-demo/src/main/java/com/dragontalker/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,33 @@
+package com.dragontalker.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import com.dragontalker.springboot.cruddemo.entity.Employee;
+
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+
+	@Override
+	public List<Employee> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
